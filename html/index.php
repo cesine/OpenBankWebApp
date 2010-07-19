ba7bb6fdedee5cf51b1da52a5385e7c898c047b4
cesine <cesine@users.noreply.github.com>
2010-07-19 04:04:31 +0000

    got the index working

---
+++ html/index.php
@@ -0,0 +1,16 @@
+<?php  include ('includes/view/header.php')?>
+
+<?php include ('includes/view/menu.php')?>
+
+<div id="content">
+<div id="main">
+	
+<h1>Hello</h1>
+<p>text text text</p>
+<!-- <img src="" width="400" height="400" alt="Place holder for main content">
+	-->
+
+</div>
+</div>
+
+<?php  include ('includes/view/footer.php')?>
