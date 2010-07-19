269de697daa70031c20b552823d69478afc36d7f
cesine <cesine@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-07-19 04:04:31 +0000

    got the index working

---
+++ html/includes/view/footer.php
@@ -0,0 +1,10 @@
+<footer>
+	<section id="about">
+	<p>&copy; Team 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco <a href="#">laboris nisi ut aliquip</a> ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+	</p>
+	</section>
+</footer>
+
+
+</body>
+</html>
---
+++ html/includes/view/menu.php
@@ -0,0 +1,19 @@
+<div id="top"> <!-- The top panel -->
+  <nav>
+    <ul>
+      <li><a href="#">Banking</a></li>
+      <li><a href="#">Borrowing</a></li>
+      <li><a href="#">Investing</a></li>
+      <li><a href="#">Insurance</a></li>
+      <li><a href="#">Menu 5</a></li>
+       <li><a href="#">Menu 6</a></li>
+      <li><a href="#">Log In</a></li>
+    </ul>
+	</nav>
+
+
+<div id="section">
+<h1>Open Bank</h1>
+<p> Online Banking Made Easy: <br/>The Open Bank sytem is designed to model the website for any Canadian bank.</p>
+</div>
+</div>
