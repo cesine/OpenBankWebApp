1a8ae4ad4b8dd8dca8688b4a59c93800797142ba
cesine <cesine@users.noreply.github.com>
2010-07-19 04:07:24 +0000

    added some text to the main content area

--- html/index.php
+++ html/index.php
@@ -6,7 +6,8 @@
 <div id="main">
 	
 <h1>Hello</h1>
-<p>text text text</p>
+<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco <a href="#">laboris nisi ut aliquip</a> ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+</p>
 <!-- <img src="" width="400" height="400" alt="Place holder for main content">
 	-->
 
