28bfa4842267cdf6c6e1e25ec2d393f109c4f51c
zayikina <zayikina@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-07-31 17:15:15 +0000

    creating view for Transaction

--- html/includes/view/Statements.php
+++ html/includes/view/Statements.php
@@ -2,6 +2,7 @@
 include('includes/model/Transaction.Class.php');
 echo '<div>';
 $transactionToDisplay = new Transaction();
+$transactionToDisplay->displayTest();
 $transactionToDisplay->displayTransaction();
 echo '</div>';
 ?>
