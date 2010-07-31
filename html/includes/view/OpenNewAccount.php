294fe10aa18793e72c8230aa68f3cf521c8c06d9
abisankar <abisankar@users.noreply.github.com>
2010-07-31 22:17:17 +0000

    addded account details

---
+++ html/includes/view/OpenNewAccount.php
@@ -0,0 +1,60 @@
+<P>Please select your choice.</P>
+<table border="1">
+<tr><td>Services:</td><td><select name="service" size="4" single="single">
+<option value="banking">Banking</option>
+<option value="investments">Investments</option>
+<option value="insurance">Insurance</option>
+<option value="borrowing">Borrowing</option>
+</select>
+</td>
+</tr></table>
+
+<P> </P>
+
+<table border="1"> 
+<tr><td>Category:</td><td><select name="serviceCategory" size="2" single="single">
+<option value="personal">Personal</option>
+<option value="business">Business</option>
+</select>
+</td>
+</tr></table>
+
+<P> </P>
+
+<table border="1"> 
+<tr><td>Type:</td><td><select name="serviceType" size="8" single="single">
+<option value="checking">Chequing</option>
+<option value="savings">Savings</option>
+<option value="foreignCurrency">Foreign Currency</option>
+<option value="creditCard">Credit cards</option>
+<option value="lineOfCredits">Line of credits</option>
+<option value="rSP">RSP</option>
+<option value="tSFA">TSFA</option>
+<option value="life">Life</option>
+</select>
+</td>
+</tr></table>
+
+<P></P>
+
+
+<table border="1"> 
+<tr><td>Account name:</td><td><select name="accountType" size="8" single="single">
+<option value="powerChecking">Powerchequing Account</option>
+<option value="moneyMasterSavings">Money Master Savings Account</option>
+<option value="usDollarAccount">US Dollar Account</option>
+<option value="creditCard">Credit Card No-Fee Value VISA</option>
+<option value="lineOfCredits">Basic Line of Credit</option>
+<option value="rSPSix">GIC(6mos) with Flex for RSPs Account</option>
+<option value="tSFA">GIC with Flex for TFSAs Account</option>
+<option value="life">Accidental Death Insurance</option>
+<option value="businessChecking">Basic Business Chequing Account</option>
+<option value="businessSavings">Basic Business Savings Account</option>
+<option value="businessFC">Basic Business Foreign Currency Account</option>
+<option value="rSPtwelve">GIC(12mos) with Flex for RSPs Account</option>
+<option value="rSPEighteen">GIC(18mos) with Flex for RSPs AccountLife</option>
+</select>
+</td>
+</tr></table>
+
+
