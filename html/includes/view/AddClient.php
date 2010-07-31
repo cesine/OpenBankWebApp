1cf1f23eb23a6574fae845d657ec6551ea49edd7
abisankar <abisankar@users.noreply.github.com>
2010-07-31 17:18:52 +0000

    to test client

--- html/includes/view/AddClient.php
+++ html/includes/view/AddClient.php
@@ -1,2 +1,9 @@
 <?php
+include('includes/model/client.Class.php');
+echo '<div>';
+$branchToDisplay = new Client();
+$branchToDisplay->test();
+$branchToDisplay->displayClientDetails();
+echo '</div>';
+
 ?>
