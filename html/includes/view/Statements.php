00f6666396f3c81afa99ed897223d22cd62c2cd4
zayikina <zayikina>
2010-07-31 18:31:44 +0000

    Show transaction in row

--- html/includes/view/Statements.php
+++ html/includes/view/Statements.php
@@ -5,4 +5,26 @@ $transactionToDisplay = new Transaction();
 $transactionToDisplay->test();
 $transactionToDisplay->displayTransaction();
 echo '</div>';
+
 ?>
+<!-- Ctreate row in table -->
+<table border = "1">
+<tr>
+<td>
+Date
+</td>
+<td>
+Desctiption
+</td>
+<td>
+Withdraw
+</td>
+<td>
+Deposits
+</td>
+<td>
+Balance
+</td>
+</tr>
+<?php $transactionToDisplay->displayTransactionInRow();?>
+</table>
