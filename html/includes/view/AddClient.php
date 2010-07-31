0f82e74441ff380dcace61ca216f1e660458c120
abisankar <abisankar@users.noreply.github.com>
2010-07-31 18:58:51 +0000

    //updated category

--- html/includes/view/AddClient.php
+++ html/includes/view/AddClient.php
@@ -16,21 +16,32 @@ echo '</div>';
 </table>
 
 <!-- Form to add a client -->
-<P>Please fill in the form below.</P>
+<P>Please fill in the details below.</P>
 <Form>
 
-First name: <INPUT type="text" name="firstName"><BR>
-Last name:<INPUT type="text" name="lastName"><BR>
-SIN: <INPUT type="text" name="socialInsuranceNumber"><BR>
-</Form>
+First name:    <INPUT type="text" name="firstName"><BR>
+Last name:     <INPUT type="text" name="lastName"><BR>
+SIN:           <INPUT type="text" name="socialInsuranceNumber"><BR>
+Date of Birth: <INPUT type="text" name="dateOfBirth"><BR>
 
+</Form>
+<P>Please select your choice.</P>
 <table border="1">
-<tr><td>Services:</td><td><select name="b" size="5" multiple="multiple">
+<tr><td>Services:</td><td><select name="service" size="4" multiple="multiple">
 
 <option value="bank   ">Banking</option>
 <option value="invest  ">Investments</option>
 <option value="insure   ">Insurance</option>
 <option value="borrow   ">Borrowing</option>
+</select>
+</td>
+</tr></table>
+<P> </P>
+<table border="1"> 
+<tr><td>Category:</td><td><select name="category" size="2" multiple="multiple">
+
+<option value="personal   ">Personal</option>
+<option value="business   ">Business</option>
 </select>
 </td>
 </tr></table>
