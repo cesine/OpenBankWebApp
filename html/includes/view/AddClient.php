baf8c99742d9186a1bba125467b1bd15e71c9d44
abisankar <abisankar@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-07-31 18:31:58 +0000

    html for add client

--- html/includes/view/AddClient.php
+++ html/includes/view/AddClient.php
@@ -7,3 +7,30 @@ $branchToDisplay->displayClientDetails();
 echo '</div>';
 
 ?>
+
+<!-- creating a table-->
+<table border="1">
+<tr><td> 1 </td><td> Hi! </td></tr>
+<tr></tr>
+<tr></tr>
+</table>
+
+<!-- Form to add a client -->
+<P>Please fill in the form below.</P>
+<Form>
+
+First name: <INPUT type="text" name="firstName"><BR>
+Last name:<INPUT type="text" name="lastName"><BR>
+SIN: <INPUT type="text" name="socialInsuranceNumber"><BR>
+</Form>
+
+<table border="1">
+<tr><td>Services:</td><td><select name="b" size="5" multiple="multiple">
+
+<option value="bank   ">Banking</option>
+<option value="invest  ">Investments</option>
+<option value="insure   ">Insurance</option>
+<option value="borrow   ">Borrowing</option>
+</select>
+</td>
+</tr></table>
