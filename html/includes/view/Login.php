927f8d483597fa0579dfa003d467ef41c6065114
cesine <cesine@users.noreply.github.com>
2010-07-31 22:30:12 +0000

    added a form for login

--- html/includes/view/Login.php
+++ html/includes/view/Login.php
@@ -1,4 +1,23 @@
 Please log in.
 <Form>
+<table width="240" border="0" align="center" cellpadding="1" cellspacing="3">
+							<tr>
+								<td class="fieldTitleLeftB">Bank Card Number</td>
 
-</Form>
+								<td><input type="text" value="453" name="69022804" size=17 maxlength=16  autocomplete="off" onKeyUp='javascript:autoTabFromSC(getFieldByNameInForm("69022804", getFormByName("LoginForm")), getFieldByNameInForm("25768052", getFormByName("LoginForm")));'></td>
+							</tr>
+							<tr>
+								<td class="fieldTitleLeftB">Password</td>
+								<td height=""><input type="password" name="25768052" size=17 maxlength=16 autocomplete="off"></td>
+							</tr>
+							<tr>
+								<td valign="middle">&nbsp;</td>
+
+								<td height="35" valign="middle">
+									<input type=image name="Submit" src="/images/enterscotiaonline.gif" width="145" height="19" alt="Enter Scotia OnLine" border="0">
+								</td>
+							</tr>
+
+
+						</table>
+
