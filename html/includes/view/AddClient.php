1974392125d9d84eacb496577a6122b4dc9085f3
abisankar <abisankar@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-07-31 19:43:47 +0000

    added type

--- html/includes/view/AddClient.php
+++ html/includes/view/AddClient.php
@@ -16,6 +16,7 @@ echo '</div>';
 </table>
 
 <!-- Form to add a client -->
+<P></P>
 <P>Please fill in the details below.</P>
 <Form>
 <table>
@@ -25,11 +26,12 @@ echo '</div>';
 <tr><td>Date of Birth:</td><td> <INPUT type="text" name="dateOfBirth"></td></tr>
 </table>
 </Form>
+
 <P> </P>
+
 <P>Please select your choice.</P>
 <table border="1">
-<tr><td>Services:</td><td><select name="service" size="4" multiple="multiple">
-
+<tr><td>Services:</td><td><select name="service" size="4" single="single">
 <option value="bank   ">Banking</option>
 <option value="invest  ">Investments</option>
 <option value="insure   ">Insurance</option>
@@ -37,12 +39,29 @@ echo '</div>';
 </select>
 </td>
 </tr></table>
-<P> </P>
-<table border="1"> 
-<tr><td>Category:</td><td><select name="category" size="2" multiple="multiple">
 
+<P> </P>
+
+<table border="1"> 
+<tr><td>Category:</td><td><select name="category" size="2" single="single">
 <option value="personal   ">Personal</option>
 <option value="business   ">Business</option>
+</select>
+</td>
+</tr></table>
+
+<P> </P>
+
+<table border="1"> 
+<tr><td>Type:</td><td><select name="type" size="8" single="single">
+<option value="checking">Chequing</option>
+<option value="savings">Savings</option>
+<option value="foreignCurrency">Foreign Currency</option>
+<option value="creditCard">Credit cards</option>
+<option value="lineOfCredits">Line of credits</option>
+<option value="rSP">RSP</option>
+<option value="tSFA">TSFA</option>
+<option value="life">Life</option>
 </select>
 </td>
 </tr></table>
