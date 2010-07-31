7f78309dcb2d49c70d7d72783311efd043f4fe04
cesine <cesine@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-07-31 16:28:06 +0000

    connected to includes

--- html/includes/model/Branch.Class.php
+++ html/includes/model/Branch.Class.php
@@ -6,7 +6,13 @@ class Branch{
 	private $managerName;
 	private $managerId;
 	private $openingDate;
+	private static $sqlTableName="branch";
+	private static $sqlTableAttributes;
 
+
+	public function getTableName() {
+		return $this->sqlTableName;
+	}
 	public function getBranchName() {
 		return $this->branchName;
 	}
@@ -19,10 +25,12 @@ class Branch{
 	public function getManagerId(){
 		return $this->managerId;
 	}
-	public function openingDate(){
+	public function getOpeningDate(){
 		return $this->openingDate;
 	}
-	
+	public function setSqlTableName($sqlTName) {
+		return $this->sqlTablesName;
+	}
 	public function setBranchName($branchName) {
 		return $this->branchName;
 	}
@@ -32,20 +40,35 @@ class Branch{
 	public function setManagerName($managerName){
 		return $this->managerName;
 	}
+	public function setManagerNameFromId(){
+		echo "Setting Manager name from ID, getting id from object, then querying DB";
+	}
+	
 	public function setManagerId($managerId){
 		return $this->managerId;
 	}
-	public function openingDate($openingDate){
+	public function setOpeningDate($openingDate){
 		return $this->openingDate;
 	}
 	
-	
+	function test(){
+		echo 'Testing the branch functionalities.';
+		$this->setBranchName("Mcgill Street");
+		$this->setManagerId(2);
+		$this->setManagerName(2);
+		$this->setManagerNameFromId();
+		$this->setOpeningDate("1992-01-01");
+		$this->setOpeningHours("Monday-Friday<br> 8am to 3pm");
+	}
 	function displayBranch(){
-		echo 'Displaying the branch.';
+		echo '<p class="name">'.$this->branchName.
+		'<br/>\n'.$this->openingHours.
+		'</p>\n\n';
 	}
 	function __construct(){
 		echo 'Creating a branch. Change somethings';
 		$this->branchName ="Metro Mcgill";
 	}
+	
 }
 ?>
