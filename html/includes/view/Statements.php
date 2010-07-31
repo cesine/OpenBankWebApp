0d6a919a18778f274f8cc2dd37e804de553e8ee5
zayikina <zayikina@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-07-31 18:49:11 +0000

    editing formatting

--- html/includes/view/Statements.php
+++ html/includes/view/Statements.php
@@ -28,3 +28,206 @@ Balance
 </tr>
 <?php $transactionToDisplay->displayTransactionInRow();?>
 </table>
+
+<table width="100%" border="0" cellpadding="3" cellspacing="1">
+
+<tbody><tr class="bgcoloroption0">
+	<td class="fieldTitleLeftC" valign="top">Date<br><span class="regularText">Sort&nbsp;</span><a class="accountLinkB" href="/portal/index.jsp?pageID=financial_services_banking&amp;?reqOption=AccountHistoryDisplay&amp;action=reDisplay&amp;FUNCTION=1&amp;accttype=DDA&amp;token=792CD57BEF9608B7B7C55AC9C5B64ACC2D30B076FBD115F7460C07D41EECFE79F1A&amp;language=English">
+
+	
+		<img src="/images/red_upTR.gif" alt="Ascending" width="13" border="0" height="9">
+	
+
+	</a><a href="/portal/index.jsp?pageID=financial_services_banking&amp;?reqOption=AccountHistoryDisplay&amp;action=reDisplay&amp;FUNCTION=3&amp;accttype=DDA&amp;token=792CD57BEF9608B7B7C55AC9C5B64ACC2D30B076FBD115F7460C07D41EECFE79F1A&amp;language=English" class="rewardsItemlink" title="Set Sort Order for all Banking Accounts">Set</a></td>
+	<td class="fieldTitleCenterC" valign="top">Num</td>
+
+	<td class="fieldTitleLeftC" valign="top">Description<br><span class="regularText">Sort&nbsp;</span><a class="accountLinkB" href="/portal/index.jsp?pageID=financial_services_banking&amp;?reqOption=AccountHistoryDisplay&amp;action=reDisplay&amp;FUNCTION=2&amp;accttype=DDA&amp;token=792CD57BEF9608B7B7C55AC9C5B64ACC2D30B076FBD115F7460C07D41EECFE79F1A&amp;language=English"><img src="/images/black_upTR.gif" alt="Ascending" width="13" border="0" height="9"></a></td>
+
+	<td class="fieldTitleRightC" valign="top">Withdrawals</td>
+	<td class="fieldTitleRightC" valign="top">Deposits</td>
+	<td class="fieldTitleRightC" valign="top">Balance</td>
+</tr>
+
+
+<tr class="bgcoloroption1" valign="top">
+	<td class="tableDataLeftC" nowrap="nowrap">2010-07-05</td>
+
+	<td class="tableDataCenterC">-</td>
+	
+		<td class="tableDataLeftC">Credit Memo<br>OTHER </td>
+	
+
+	
+		<td class="tableDataCenterC">-</td>
+	
+	
+		<td class="tableDataRightC" align="right">
+     		$700.00
+  		</td>
+	
+	
+		
+		   <td class="tableDataRightC" align="right">
+
+		     
+     			$4,890.80
+		     
+		  </td>
+	    	
+	
+</tr>
+
+
+<tr class="bgcoloroption2" valign="top">
+	<td class="tableDataLeftC" nowrap="nowrap">2010-07-06</td>
+	<td class="tableDataCenterC">-</td>
+	
+		<td class="tableDataLeftC">Customer Transfer Dr.<br>PC TO 4538156430915 </td>
+
+	
+
+	
+		<td class="tableDataRightC" align="right">
+		$4,190.00
+		</td>
+	
+	
+		<td class="tableDataCenterC">-</td>
+	
+	
+		
+		   <td class="tableDataCenterC">-</td>
+		
+	
+</tr>
+
+
+<tr class="bgcoloroption1" valign="top">
+	<td class="tableDataLeftC" nowrap="nowrap">2010-07-06</td>
+
+	<td class="tableDataCenterC">-</td>
+	
+		<td class="tableDataLeftC">Customer Transfer Dr.<br>PC TO 4538156430915 </td>
+	
+
+	
+		<td class="tableDataRightC" align="right">
+		$10.00
+		</td>
+	
+	
+		<td class="tableDataCenterC">-</td>
+	
+	
+		
+		   <td class="tableDataRightC" align="right">
+
+		     
+     			$690.80
+		     
+		  </td>
+	    	
+	
+</tr>
+
+
+<tr class="bgcoloroption2" valign="top">
+	<td class="tableDataLeftC" nowrap="nowrap">2010-07-13</td>
+	<td class="tableDataCenterC">-</td>
+	
+		<td class="tableDataLeftC">Deposit<br> </td>
+
+	
+
+	
+		<td class="tableDataCenterC">-</td>
+	
+	
+		<td class="tableDataRightC" align="right">
+     		$4,515.00
+  		</td>
+	
+	
+		
+		   <td class="tableDataRightC" align="right">
+		     
+     			$5,205.80
+		     
+		  </td>
+	    	
+	
+</tr>
+
+
+<tr class="bgcoloroption1" valign="top">
+	<td class="tableDataLeftC" nowrap="nowrap">2010-07-14</td>
+	<td class="tableDataCenterC">2</td>
+	
+		<td class="tableDataLeftC"><a class="accountLinkB" title="View Cheque Image" href="/portal/index.jsp?pageID=financial_services_banking&amp;subPageId=Banking_Accounts&amp;reqOption=ChqImgDisclaimer&amp;src=AccountHistory&amp;an=DDA;CA;DDA;CA;CAD;3648.09999999999990905052982270717620849609375;2322629620217624967@3708478640705414144&amp;idx=4">Cheque</a><br> </td>
+	
+
+	
+		<td class="tableDataRightC" align="right">
+		$669.70
+		</td>
+	
+	
+		<td class="tableDataCenterC">-</td>
+
+	
+	
+		
+		   <td class="tableDataRightC" align="right">
+		     
+     			$4,536.10
+		     
+		  </td>
+	    	
+	
+</tr>
+
+
+
+
+
+<tr class="bgcoloroption1" valign="top">
+	<td class="tableDataLeftC" nowrap="nowrap">2010-07-26</td>
+	<td class="tableDataCenterC">-</td>
+	
+		<td class="tableDataLeftC">Deposit<br> </td>
+	
+
+	
+		<td class="tableDataCenterC">-</td>
+	
+	
+		<td class="tableDataRightC" align="right">
+     		$3,612.00
+  		</td>
+
+	
+	
+		
+		   <td class="tableDataRightC" align="right">
+		     
+     			$3,648.10
+		     
+		  </td>
+	    	
+	
+</tr>
+
+
+
+	<tr class="bgcoloroption2" valign="top">
+	  <td colspan="3" class="fieldTitleRightC">Total
+
+		:</td>
+	  <td class="totalBalanceC" align="right">&nbsp;$9,369.70</td>
+
+	  <td class="totalBalanceC" align="right">&nbsp;$8,827.00</td>
+	  <td class="tableDataCenterC">&nbsp;</td>
+	</tr>
+	
+
+</tbody></table>
