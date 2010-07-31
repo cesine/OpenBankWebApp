d60658b8335b24edeade93ec61a2ff5e2561842a
abisankar <abisankar@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-07-31 17:12:45 +0000

    implemented client class

--- html/includes/model/Client.Class.php
+++ html/includes/model/Client.Class.php
@@ -1,2 +1,111 @@
 <?php 
-for bankapp - client class?>
+class Client{
+	private $clientID;
+	private $firstName;
+	private $lastName;
+	private $socialInsuranceNumber;
+	private $dateOfBirth;
+	private $startDate;
+	private $status;
+	private $addressID;
+	private $branchID;	//maybe we need
+	private $clientAccountID;	//maybe we need
+	private static $sqlTableName="client";
+	private static $sqlTableAttributes;
+	
+	//getters
+	public function getTableName() {
+		return $this->sqlTableName;
+	}
+	public function getClientID() {
+		return $this->clientID;
+	}
+	public function getFirstName() {
+		return $this->firstName;
+	}
+	public function getLastName() {
+		return $this->lastName;
+	}
+	public function getSocialInsuranceNumber() {
+		return $this->socialInsuranceNumber;
+	}
+	public function getDateOfBirth() {
+		return $this->dateOfBirth;
+	}
+	public function getStartDate() {
+		return $this->startDate;
+	}
+	public function getStatus() {
+		return $this->status;
+	}
+	public function getAddressID() {
+		return $this->addressID;
+	}
+	public function getBranchID() {
+		return $this->branchID;
+	}
+	public function getClientAccountID() {
+		return $this->clientAccountID;
+	}
+	
+	//setters
+	public function setSqlTableName($sqlTName) {
+		$this->sqlTablesName=$sqlTName;
+	}
+	public function setClientID($clientID) {
+		$this->clientID=$clientID;
+	}
+	public function setFirstName($firstName) {
+		$this->firstName=$firstName;
+	}
+	public function setLastName($lastName) {
+		$this->lastName=$lastName;
+	}
+	public function setSocialInsuranceNumber($socialInsuranceNumber) {
+		$this->socialInsuranceNumber=$socialInsuranceNumber;
+	}
+	public function setDateOfBirth($dateOfBirth) {
+		$this->dateOfBirth=$dateOfBirth;
+	}
+	public function setStartDate($startDate) {
+		$this->startDate=$startDate;
+	}
+	public function setStatus($status) {
+		$this->status=$status;
+	}
+	public function setAddressID($addressID) {
+		$this->addressID=$addressID;
+	}
+	public function setBranchID($branchID) {
+		$this->branchID=$branchID;
+	}
+	public function setClientAccountID($clientAccountID) {
+		$this->clientAccountID=$clientAccountID;
+	}
+	
+	function test(){
+		echo 'Testing the client functionalities.';
+		$this->setClientID(12300045);
+		$this->setFirstName("John");
+		$this->setLastName("Blake");
+		$this->setSocialInsuranceNumber("875 345 281");
+		$this->setDateOfBirth("1954-05-21");
+		$this->setStartDate("2010-03-15");
+		$this->setStatus(1);
+		$this->setAddressID(10000049);
+		$this->setBranchID(1001);
+		$this->setClientAccountID(95432453);
+	}
+	function displayBranch(){
+		echo '<p class="name">'.$this->clientID.
+		'<br/>\n'.$this->firstName.
+		'<br/>\n'.$this->lastName.
+		'</p>\n\n';
+	}
+	function __construct(){
+		echo 'Adding a client, changing firstName';
+		$this->firstName="Jack";
+	}
+}
+
+?>
