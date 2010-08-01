a11f215c1c24dd1f867b7cecfc1b70bcaee6318a
cesine <cesine@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-01 02:11:01 +0000

    added menus for the Client and Employee version of the Main content area, also added if statements in the index to control the display of these pages based on who is logged in (at the moment through the URl and $_GET, but later though the $_SESSION)

---
+++ html/includes/view/clienttopmenu.php
@@ -0,0 +1,5 @@
+<table width="600" border="0" bgcolor="#bbb" ><tr><td width="20%">
+<a href="index.php?&contents=AccountsSummary">Accounts Summary</a></td><td width="20%">
+<a href="">Bill Payments</a></td><td width="20%">
+<a href="">Transfers</a></td><td width="20%">
+<a href="">More Features</a></td></tr></table>
---
+++ html/includes/view/employeetopmenu.php
@@ -0,0 +1,5 @@
+<table  width="600" border="0" bgcolor="#bbb" ><tr><td>
+<a href="">Add Employee</a></td><td>
+<a href="">View a Client's Info</a></td><td>
+<a href="">View Branch Info</a></td><td>
+<a href="">View my Employee Info</a></td></tr></table>
