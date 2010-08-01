c768e89d049c39b85a42b36ae5379b57d5ea0c90
cesine <cesine@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-01 04:53:04 +0000

    added database class and config file which contains the connection info, implemented some sample use of the database in the branch locator's test (examples: create db object, open a connection, run a query, close the db)  and setManagerName function (extract info from a query into an object's member variable)

---
+++ html/includes/controller/configMySQL.php
@@ -0,0 +1,34 @@
+<?php
+
+define('DB_SERVER', "localhost");
+define('DB_USER',"openbank");
+define('DB_PASS', "ShDxutQDACnrRjCW");
+define('DB_DATABASE',"bankapp");
+
+
+
+/*
+ * Table definitions so that the constant can be set here once, and reused in numerous 
+ * SQL queries in many pages
+ */
+define('TABLE_ACCOUNTTYPE',"accounttype");
+define('TABLE_ADDRESS',"address");
+define('TABLE_BANKINGPLANS',"bankingplans");
+define('TABLE_BORROWINGPLANS',"borrowingplans");
+define('TABLE_BRANCH',"branch");
+define('TABLE_CLIENT',"client");
+define('TABLE_CLIENTACCOUNT',"clientaccount");
+define('TABLE_CLIENTLOGIN',"clientlogin");
+define('TABLE_EMPLOYEE',"employee");
+define('TABLE_EMPLOYEELOGIN',"employeelogin");
+define('TABLE_EMPLOYEETIMEOFFHISTORY',"employeetimeoffhistory");
+define('TABLE_EMPLOYEETITLE',"employeetitle");
+define('TABLE_EMPLOYEEWORKHISTORY',"employeeworkhistory");
+define('TABLE_INSURANCEPLANS',"insuranceplans");
+define('TABLE_INVESTMENTPLANS',"investmentplans");
+define('TABLE_SERVICE',"service");
+define('TABLE_SERVICECATEGORY',"servicecategory");
+define('TABLE_SERVICETYPE',"servicetype");
+define('TABLE_TRANSACTION',"transaction");                  
+
+?>
--- html/includes/model/Branch.Class.php
+++ html/includes/model/Branch.Class.php
@@ -41,6 +41,20 @@ class Branch{
 	}
 	public function setManagerNameFromId(){
 		echo "Setting Manager name from ID, getting id from object, then querying DB";
+		$db = new Database();
+		$db->connect();
+		$queryString= "SELECT DISTINCT firstname,lastname FROM employee e	WHERE e.employeeid=20000005";
+		$db->query($queryString);
+		//$db->query("SELECT * FROM branch",$db->link_id);
+		/*
+		 * Loop through the results (there should only be one result)
+		 * 
+		 * 		access the results via the function mysql_fetch_array() which will turn the results into an array 
+		 */
+		print_r($db->queryResults); //debuging, print out the array to find the element you need
+		$this->managerName=$db->queryResults[firstname]." ".$db->queryResults[lastname];
+		//echo $this->managerName;
+		
 	}
 	
 	public function setManagerId($managerIdIn){
@@ -51,17 +65,30 @@ class Branch{
 	}
 	
 	function test(){
-		echo 'Testing the branch functionalities.';
+		echo 'Testing the branch functionalities.<p>';
 		$this->setBranchName("Mcgill Street");
 		$this->setManagerId(2);
 		$this->setManagerName(2);
 		$this->setManagerNameFromId();
 		$this->setOpeningDate("1992-01-01");
 		$this->setOpeningHours("Monday-Friday<br> 8am to 3pm");
+		$this->displayBranch();
+		
+		
+		echo 'Testing the database connection.';
+		echo DB_USER; //this is a constant set by the configMySql file, if the word "openbank" appears
+						// it means that the config file was sucessfully included. 
+		$db = new Database();//this creates a database object
+		$dbConnectionNumber = $db->connect(); //this attempts to create a connection the datbase and puts the connection's 
+												//number into $dbConnectionNumber
+		$db->query("SELECT * FROM branch");//this sends the query to the database on the database object's link number
+		print_r($db->queryResults);	//this prints an array, namely the array which was put into the database object's queryResults variable
+		$db->close();//this closes the dtabase link connection
 	}
 	function displayBranch(){
 		echo '<p class="name">'.$this->branchName.
-		'<br/>'.$this->openingHours.
+		'</b><p><b>Branch Manager:</b>'.$this->managerName.
+		'</p><p>'.$this->openingHours.
 		'</p>';
 	}
 	function __construct(){
