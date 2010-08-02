37e357498492f677fc32c7b65b6a9d26f880305d
cesine <cesine@users.noreply.github.com>
2010-08-02 17:12:16 +0000

    backingup database

--- databasebackup/bankapp.sql
+++ databasebackup/bankapp.sql
@@ -78,13 +78,13 @@ DROP TABLE IF EXISTS `bankingplans`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bankingplans` (
   `accounttypeid` int(2) unsigned NOT NULL,
-  `monthlyfee` double NOT NULL,
+  `monthlyfee` decimal(3,2) NOT NULL,
   `freetransactions` int(3) unsigned NOT NULL,
-  `transactionfee` double NOT NULL,
-  `overdraftamount` double NOT NULL,
-  `overdraftprotectionfee` double NOT NULL,
-  `minimumbalance` decimal(10,0) NOT NULL,
-  `interestrate` int(11) NOT NULL,
+  `transactionfee` decimal(2,2) NOT NULL,
+  `overdraftamount` decimal(5,2) NOT NULL,
+  `overdraftprotectionfee` decimal(3,2) NOT NULL,
+  `minimumbalance` decimal(10,2) NOT NULL,
+  `interestrate` decimal(2,2) NOT NULL,
   UNIQUE KEY `accounttypeid` (`accounttypeid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -95,7 +95,7 @@ CREATE TABLE `bankingplans` (
 
 LOCK TABLES `bankingplans` WRITE;
 /*!40000 ALTER TABLE `bankingplans` DISABLE KEYS */;
-INSERT INTO `bankingplans` VALUES (1,4,20,0.65,1000,5,'0',0),(3,25,10,1,0,0,'0',0);
+INSERT INTO `bankingplans` VALUES (1,'4.00',20,'0.65','999.99','5.00','0.00','0.00'),(3,'9.99',10,'0.99','0.00','0.00','0.00','0.00');
 /*!40000 ALTER TABLE `bankingplans` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -108,10 +108,10 @@ DROP TABLE IF EXISTS `borrowingplans`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `borrowingplans` (
   `accounttypeid` int(2) unsigned NOT NULL,
-  `creditlimit` double NOT NULL,
-  `monthlyfee` double NOT NULL,
+  `creditlimit` decimal(10,2) NOT NULL,
+  `monthlyfee` decimal(3,2) NOT NULL,
   `graceperiod` int(2) unsigned NOT NULL,
-  `interestrate` double NOT NULL,
+  `interestrate` decimal(2,2) NOT NULL,
   UNIQUE KEY `accounttypeid` (`accounttypeid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -135,14 +135,11 @@ DROP TABLE IF EXISTS `branch`;
 CREATE TABLE `branch` (
   `branchid` int(5) unsigned NOT NULL,
   `branchname` varchar(12) NOT NULL,
-  `addressid` int(8) unsigned NOT NULL,
   `managerid` int(8) unsigned NOT NULL,
   `openingdate` date NOT NULL,
   `openinghours` tinytext NOT NULL,
   `branchsclientid` int(8) unsigned NOT NULL,
-  `status` int(1) unsigned NOT NULL,
   PRIMARY KEY (`branchid`),
-  KEY `addressid` (`addressid`),
   KEY `managerid` (`managerid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -153,7 +150,7 @@ CREATE TABLE `branch` (
 
 LOCK TABLES `branch` WRITE;
 /*!40000 ALTER TABLE `branch` DISABLE KEYS */;
-INSERT INTO `branch` VALUES (10001,'Downtown Mtl',10000001,2147483647,'1990-01-01','Mon-Fri 10:00-17:00',0,0),(10002,'Montreal W.I',10000002,2147483647,'1992-01-01','Mon-Fri 10:00-17:00',0,0),(10003,'Downtown T.O',10000003,2147483647,'1994-01-01','Mon-Fri 10:00-17:00',0,0),(10004,'Quebec QC',10000004,2147483647,'1995-01-01','Mon-Fri 10:00-17:00',0,0);
+INSERT INTO `branch` VALUES (10001,'Downtown Mtl',20000001,'1990-01-01','Mon-Fri 10:00-17:00',54010001),(10002,'Montreal W.I',20000002,'1992-01-01','Mon-Fri 10:00-17:00',54010002),(10003,'Downtown T.O',20000003,'1994-01-01','Mon-Fri 10:00-17:00',54010003),(10004,'Quebec QC',20000004,'1995-01-01','Mon-Fri 10:00-17:00',54010004);
 /*!40000 ALTER TABLE `branch` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -175,7 +172,7 @@ CREATE TABLE `client` (
   `status` int(1) NOT NULL,
   PRIMARY KEY (`clientid`),
   KEY `addressid` (`addressid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=54010023 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -184,6 +181,7 @@ CREATE TABLE `client` (
 
 LOCK TABLES `client` WRITE;
 /*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` VALUES (54010001,'124 231 435','Downtown','Mtl',10000001,'1990-01-01','1990-01-01',1),(54010002,'156 345 234','Montreal ','W.I',10000002,'1992-01-01','1992-01-01',1),(54010003,'176 453 875','Downtown ','T.O',10000003,'1994-01-01','1994-01-01',1),(54010004,'199 874 561','Quebec',' QC',10000004,'1995-01-01','1995-01-01',1),(54010005,'214 586 216','Carla','Suarez',10000005,'1964-12-30','1990-11-05',1),(54010006,'218 741 532','Daniela','Hantuchova',10000006,'1972-11-08','2001-12-01',1),(54010007,'241 257 845','Flavia','Pennetta',10000007,'1959-02-14','2000-04-08',1),(54010008,'221 963 548','Arantxa','Parra',10000008,'1969-09-04','2003-10-24',1),(54010009,'254 284 541','Alisa','Kleybanova',10000009,'1982-02-21','2007-05-21',1),(54010010,'241 653 523','Kateryna','Bondarenko',10000010,'1979-02-11','1999-10-18',1),(54010011,'287 243 097','Caroline','Wozniacki',10000013,'1988-06-28','2003-09-07',1),(54010012,'298 835 908','Yanina','Wickmayer',10000016,'1990-08-03','2008-11-04',0),(54010013,'187 674 323','Mountainlake','University',10000041,'1954-05-11','1990-01-24',1),(54010014,'129 340 002','Highhill','Hospital',10000042,'1919-01-02','1991-02-17',1),(54010015,'265 336 987','Chantal','Wordsworth',10000043,'1945-07-05','1999-09-09',0),(54010016,'213 654 147','Oscar','Wilde',10000044,'1984-12-23','1998-03-20',1),(54010017,'284 149 589','Chander','Sai',10000045,'1977-04-28','2002-08-31',1),(54010018,'216 563 672','Web Weaver','Consultancy',10000046,'1999-08-05','1999-08-05',1),(54010019,'129 342 100','Adminstration',' Highhill Hospital',10000047,'1919-01-02','1991-02-17',1),(54010020,'229 058 741','Paulette','Bourgeois',10000048,'1970-02-16','2005-07-13',1),(54010021,'287 674 390','Human Resourses',' Mountainlake Univ.',10000049,'1954-05-11','1990-01-24',1),(54010022,'148 587 377','Edison','Albert',10000050,'1945-11-07','1999-08-14',1);
 /*!40000 ALTER TABLE `client` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -199,8 +197,8 @@ CREATE TABLE `clientaccount` (
   `branchid` int(5) unsigned NOT NULL,
   `clientid` int(8) unsigned NOT NULL,
   `accounttypeid` int(2) unsigned NOT NULL,
-  `currentbalance` double NOT NULL,
-  `availablebalance` double NOT NULL,
+  `currentbalance` decimal(10,2) NOT NULL,
+  `availablebalance` decimal(10,2) NOT NULL,
   `status` int(1) NOT NULL,
   `openingdate` date NOT NULL,
   `closingdate` date DEFAULT NULL,
@@ -215,6 +213,7 @@ CREATE TABLE `clientaccount` (
 
 LOCK TABLES `clientaccount` WRITE;
 /*!40000 ALTER TABLE `clientaccount` DISABLE KEYS */;
+INSERT INTO `clientaccount` VALUES (10000001,10001,54010001,2,'3500.00','3500.00',1,'2010-07-01',NULL),(10000002,10001,54010001,1,'700.00','700.00',1,'2010-06-01',NULL),(10000003,10001,54010001,2,'4000.00','4000.00',1,'2010-01-01',NULL),(20000002,10001,54010002,2,'6500.00','6500.00',1,'2010-06-01',NULL),(110001001,10001,54010005,1,'4205.65','3205.65',1,'1990-11-05',NULL),(110001002,10001,54010009,1,'4800.30','3800.30',1,'2007-05-21',NULL),(110001003,10001,54010011,1,'2125.50','1125.50',1,'2003-09-07',NULL),(110001012,10001,54010015,1,'0.00','0.00',0,'1999-09-09','2007-05-01'),(110001019,10001,54010016,2,'14900.00','14900.00',1,'1998-03-20',NULL),(110001022,10001,54010015,2,'4500.00','4500.00',1,'2007-05-01',NULL),(110001039,10001,54010022,1,'3290.85','2290.85',1,'1999-08-14',NULL),(110001090,10001,54010020,1,'5690.00','4690.00',1,'2005-07-13',NULL),(110001102,10001,54010017,2,'12000.00','12000.00',1,'2002-08-31',NULL),(110002001,10002,54010006,1,'6456.13','5456.13',1,'2001-12-01',NULL),(110002002,10002,54010010,1,'2750.00','1750.00',1,'1999-10-18',NULL),(110003001,10003,54010007,1,'2100.00','1100.00',1,'2000-04-08',NULL),(110004001,10004,54010008,1,'5089.67','4089.67',1,'2003-10-24',NULL),(110004002,10004,54010012,1,'0.00','0.00',0,'2008-11-04','2010-07-28'),(110004523,10004,54010008,7,'20000.00','20000.00',1,'2010-07-20',NULL),(210001004,10001,54010013,11,'76239.05','76239.05',1,'1990-01-24',NULL),(210001005,10001,54010014,11,'34765.35','34765.35',1,'1991-02-17',NULL),(210001006,10001,54010019,10,'12980.99','10980.99',1,'1991-02-17',NULL),(210001007,10001,54010021,10,'18540.45','16540.45',1,'1990-01-24',NULL),(210001014,10001,54010018,11,'45000.00','45000.00',1,'1999-08-05',NULL),(210002001,10002,54010002,10,'86543.98','84543.98',1,'1992-01-01',NULL),(210003001,10003,54010003,10,'75089.07','73089.07',1,'1994-01-01',NULL),(210004001,10004,54010004,10,'26403.68','24403.68',1,'1995-01-01',NULL);
 /*!40000 ALTER TABLE `clientaccount` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -238,6 +237,7 @@ CREATE TABLE `clientlogin` (
 
 LOCK TABLES `clientlogin` WRITE;
 /*!40000 ALTER TABLE `clientlogin` DISABLE KEYS */;
+INSERT INTO `clientlogin` VALUES (54010001,'BroneDTm'),(54010002,'BrtwoWIm'),(54010003,'Br3dtto!'),(54010004,'Suc4Qccv'),(54010005,'hook23th'),(54010006,'yu87?E3R'),(54010007,'12Pu876BN'),(54010008,'su*gh45Y'),(54010009,'TR678$cad'),(54010010,'moneyMind'),(54010011,'Invest$@b'),(54010012,'try2bank!'),(54010013,'8A$$&pkebt'),(54010014,'we56#4have'),(54010015,'Hi89R!23'),(54010016,'hj67@gooey'),(54010017,'all4*moni!'),(54010018,'78babyto90'),(54010019,'root#beer'),(54010020,'alors9AC'),(54010021,'onYva96*'),(54010022,'45&ltrue');
 /*!40000 ALTER TABLE `clientlogin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -324,6 +324,30 @@ LOCK TABLES `employeetimeoffhistory` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `employeetimeoffplan`
+--
+
+DROP TABLE IF EXISTS `employeetimeoffplan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employeetimeoffplan` (
+  `timeoffid` int(5) NOT NULL,
+  `timeoffname` varchar(20) NOT NULL,
+  `numberofdays` int(2) NOT NULL,
+  PRIMARY KEY (`timeoffid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employeetimeoffplan`
+--
+
+LOCK TABLES `employeetimeoffplan` WRITE;
+/*!40000 ALTER TABLE `employeetimeoffplan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employeetimeoffplan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `employeetitle`
 --
 
@@ -333,7 +357,7 @@ DROP TABLE IF EXISTS `employeetitle`;
 CREATE TABLE `employeetitle` (
   `titleid` int(2) NOT NULL,
   `titlename` varchar(25) NOT NULL,
-  `basesalary` double NOT NULL,
+  `basesalary` decimal(8,2) NOT NULL,
   PRIMARY KEY (`titleid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -344,7 +368,7 @@ CREATE TABLE `employeetitle` (
 
 LOCK TABLES `employeetitle` WRITE;
 /*!40000 ALTER TABLE `employeetitle` DISABLE KEYS */;
-INSERT INTO `employeetitle` VALUES (10,'Branch Manager',100000),(15,'Assistant Manager',70000),(20,'Banking Consultant',55000),(30,'Teller',30000);
+INSERT INTO `employeetitle` VALUES (10,'Branch Manager','100000.00'),(15,'Assistant Manager','70000.00'),(20,'Banking Consultant','55000.00'),(30,'Teller','30000.00');
 /*!40000 ALTER TABLE `employeetitle` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -361,7 +385,7 @@ CREATE TABLE `employeeworkhistory` (
   `startdate` date NOT NULL,
   `lastdate` date NOT NULL,
   `titleid` int(2) unsigned NOT NULL,
-  `salary` double NOT NULL,
+  `salary` decimal(8,2) NOT NULL,
   PRIMARY KEY (`employeeid`,`branchid`,`startdate`,`lastdate`,`titleid`,`salary`),
   KEY `branchid` (`branchid`),
   KEY `titleid` (`titleid`)
@@ -386,8 +410,8 @@ DROP TABLE IF EXISTS `insuranceplans`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `insuranceplans` (
   `accounttypeid` int(2) unsigned NOT NULL,
-  `coverage` double NOT NULL,
-  `monthlypremium` double NOT NULL,
+  `coverage` decimal(10,2) NOT NULL,
+  `monthlypremium` decimal(8,2) NOT NULL,
   UNIQUE KEY `accounttypeid` (`accounttypeid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -411,7 +435,7 @@ DROP TABLE IF EXISTS `investmentplans`;
 CREATE TABLE `investmentplans` (
   `accounttypeid` int(2) unsigned NOT NULL,
   `investmentterm` int(3) unsigned NOT NULL,
-  `interestrate` double NOT NULL,
+  `interestrate` decimal(2,2) NOT NULL,
   UNIQUE KEY `accounttypeid` (`accounttypeid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -422,7 +446,7 @@ CREATE TABLE `investmentplans` (
 
 LOCK TABLES `investmentplans` WRITE;
 /*!40000 ALTER TABLE `investmentplans` DISABLE KEYS */;
-INSERT INTO `investmentplans` VALUES (6,6,1),(13,12,1.75),(14,18,2);
+INSERT INTO `investmentplans` VALUES (6,6,'0.99'),(13,12,'0.99'),(14,18,'0.99');
 /*!40000 ALTER TABLE `investmentplans` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -511,11 +535,11 @@ CREATE TABLE `transaction` (
   `clientid` int(8) unsigned NOT NULL,
   `accountid` int(8) unsigned NOT NULL,
   `date` date NOT NULL,
-  `transactionfeecharged` double DEFAULT NULL,
+  `transactionfeecharged` decimal(2,2) DEFAULT NULL,
   `transactionfeetype` varchar(25) NOT NULL,
-  `depositamount` double DEFAULT NULL,
-  `withdrawalamount` double DEFAULT NULL,
-  `balance` double NOT NULL,
+  `depositamount` decimal(10,2) DEFAULT NULL,
+  `withdrawalamount` decimal(8,2) DEFAULT NULL,
+  `balance` decimal(10,2) NOT NULL,
   `transactiondescription` varchar(50) NOT NULL,
   PRIMARY KEY (`transactionid`),
   KEY `accountid` (`accountid`),
@@ -529,7 +553,7 @@ CREATE TABLE `transaction` (
 
 LOCK TABLES `transaction` WRITE;
 /*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
-INSERT INTO `transaction` VALUES (1,2,3,'2010-07-31',3,'monthly fee',2000,0,2000,'my second deposit'),(11,22,333,'2010-07-31',5,'monthly fee',0,1000,1000,'my first deposit');
+INSERT INTO `transaction` VALUES (27,54010015,110001012,'2007-05-01','0.00','Free transaction',NULL,'4500.00','0.00','Account transfer; Closed account '),(28,54010015,110001022,'2007-05-01','0.00','Free transaction','4500.00',NULL,'4500.00','Opened account tranferring money'),(231,54010012,110004002,'2010-07-28','0.00','Free transaction',NULL,'1454.56','0.00','On ATM; Closed account '),(476,54010018,210001014,'2010-07-30','0.99','Service charges','33000.00','0.00','72000.00','Check deposit on Mtl branch'),(531,54010022,110001039,'2010-07-28','0.00','Free transaction',NULL,'124.00','7987.00','Bill payment on ATM; To HydroQuebec'),(543,54010020,110001090,'2010-07-25','0.00','Free transaction',NULL,'79.00','4308.61','Bill payment online; To HydroQuebec'),(558,54010016,110001019,'2010-08-01','0.00','Free transaction',NULL,'1329.00','1567.00','Bill payment by telephone; To Mountainlake Univ.'),(559,54010013,210001004,'2010-08-02','0.00','Free transaction','1329.00',NULL,'34454.56','Received payment by telephone; From Oscar Wilde'),(91100001,54010001,10000001,'2010-07-01','0.00','free transaction','5000.00',NULL,'5000.00','my first deposit on saving account'),(91100002,54010001,10000001,'2010-07-05','0.00','free transaction',NULL,'1000.00','4000.00','withdraw money for trip'),(91100003,54010001,10000001,'2010-07-10','0.00','free transaction',NULL,'1000.00','3000.00','withdraw money for trip'),(91100004,54010001,10000001,'2010-07-15','0.00','free transaction','500.00',NULL,'3500.00','deposit money left from trip'),(91200001,54010001,10000002,'2010-06-01','0.00','free transaction','1000.00',NULL,'1000.00','my first deposit on checking account'),(91200002,54010001,10000002,'2010-06-02','0.00','free transaction',NULL,'100.00','900.00','bill payment'),(91200003,54010001,10000002,'2010-06-03','0.00','free transaction',NULL,'200.00','700.00','bill payment'),(91200004,54010001,10000002,'2010-06-10','0.00','free transaction',NULL,'500.00','200.00','bill payment'),(91200005,54010001,10000002,'2010-06-15','0.00','free transaction','500.00',NULL,'700.00','my second deposit on checking account'),(91300001,54010001,10000003,'2010-01-01','0.00','free transaction','4000.00',NULL,'4000.00','deposit money for future studing');
 /*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -542,4 +566,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-07-31 17:03:01
+-- Dump completed on 2010-08-02 13:12:06
