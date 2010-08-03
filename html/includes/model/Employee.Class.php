dffa318dd3b181babf69e75108036b3e850f5a25
zayikina <zayikina>
2010-08-03 18:30:21 +0000

    addig functionality to Employee

--- html/includes/model/Employee.Class.php
+++ html/includes/model/Employee.Class.php
@@ -103,6 +103,38 @@ class Account{
 	}	
 	
 	
+	function displayEmployeeInRowFormatted()
+	{
+		
+	echo '<TR class="bgcoloroption1">
+	<TD class="tableDataLeftC">'.$this->employeeID.'</TD>
+	<TD class="tableDataRightC">'.$this->firstName.'</TD>
+	<TD class="tableDataRightC">'.$this->lastName.'</TD>	
+	<TD class="tableDataRightC">'.$this->branchID.'</TD>	
+	<TD class="tableDataRightC">'.$this->titleID.'</TD>		
+	<TD class="tableDataLeftC">'.$this->salary.'</TD>
+	<TD class="tableDataRightC">'.$this->addressID.'</TD>	
+	<TD class="tableDataRightC">'.$this->timeOffID.'</TD>
+	<TD class="tableDataRightC">'.$this->status.'</TD>	
+
+
+	</TR>';		
+	}
+
+	public function initializeEmployee($row)
+	{
+		// in the line ($row[employeeid]), parameter name [] from db table
+		$this->setEmployeeID($row[employeeid]);
+		$this->setFirstName($row[firstname]);
+		$this->setLastName($row[lastname]);	
+		$this->setBranchID($row[branchid]);	
+		$this->setTitleID($row[titleid]);				
+		$this->setSalary($row[salary]);
+		$this->setAddressID($row[addressid]);	
+		$this->setTimeOffID($row[timeoffid]);
+		$this->setStatus($row[status]);
+	}	
+	
 }
 ?>
 
