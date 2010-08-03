079123026563b3a7ff76462057429349f60f27a4
zayikina <zayikina>
2010-08-03 14:36:38 +0000

    editing EmployeeLogin page

--- html/includes/view/EmployeeLogin.php
+++ html/includes/view/EmployeeLogin.php
@@ -1,1 +1,24 @@
-<?php
+Please log in.
+<form>
+
+	<table width="240" border="0" align="center" cellpadding="1" cellspacing="3">
+	
+		<tr>
+		
+			<td class="fieldTitleLeftB">Employee ID:</td>
+			<td><input type="text" name="EmployeeID" size="20" maxlength="8" value=""/></td>
+			
+		</tr>
+	
+		<tr>
+		
+			<td valign="middle">&nbsp;</td>
+			
+			<td height="35" valign="middle">
+			<input type="submit" name="EmployeeIDSubmit" value="EmployeeLogin" />
+			</td>
+		</tr>
+	
+	</table>
+
+</form>
