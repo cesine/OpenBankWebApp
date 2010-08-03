1a1a8ccf2a2c24cf8784dc3bc947db90b883d9fa
zayikina <zayikina>
2010-08-03 15:22:41 +0000

    verification employee ID

--- html/includes/view/EmployeeLogin.php
+++ html/includes/view/EmployeeLogin.php
@@ -1,3 +1,47 @@
+<?php 
+
+session_start();
+
+include('includes/model/Employee.Class.php');
+include('includes/controller/Database.Class.php');
+
+//echo '<div>';
+//$transactionToDisplay = new Transaction();
+//echo '</div>';
+
+//if login button is pressed:
+if (isset($_POST['EmployeeIDSubmit']))
+{
+	//to check if employee ID is provided:
+    if(($_POST['EmployeeID'] == ''))
+    {	
+    	echo '<p class="err_mess">Please, provide employee ID.</p>';
+    }
+	else
+	{
+		//to check if employee ID exist in DB 
+		$dbCheckEmployeeID = new Database();
+		$dbCheckEmployeeID->connect();
+		
+		$queryCheckEmployeeID="SELECT DISTINCT $EmployeeID 
+							   FROM employee";
+									
+		$dbCheckEmployeeID->query($queryCheckEmployeeID);	
+		$result = $dbCheckEmployeeID->query($queryCheckEmployeeID);
+
+		if(($dbCheckEmployeeID->queryResultsCount)==1)
+		{
+			echo "<h4> Employee found in DB. </h4>\n";
+		}
+		else
+		{
+			echo "<h4> Employee ID: $EmployeeID not exist in DB !</h4>\n";			
+		}
+	}
+} // end if (isset($_POST['EmployeeIDSubmit']))
+
+?>
+
 <p></p>
 <?php 
 echo "<h5> Please log in. </h5>\n";
