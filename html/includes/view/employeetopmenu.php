ab8a43af660929857f5e2e470a03e7391088d8f5
zayikina <zayikina@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-03 18:56:09 +0000

    correct employee top menu

--- html/includes/view/employeetopmenu.php
+++ html/includes/view/employeetopmenu.php
@@ -1,5 +1,5 @@
 <table  width="600" border="0" bgcolor="#bbb" ><tr><td>
-<a href="">Add Employee</a></td><td>
-<a href="">View a Client's Info</a></td><td>
-<a href="">View Branch Info</a></td><td>
-<a href="">View my Employee Info</a></td></tr></table>
+<a href="">Employees Info</a></td><td>
+<a href="">Clients Info</a></td><td>
+<a href="">Branch Info</a></td><td>
+<a href="">My Employee Info</a></td></tr></table>
