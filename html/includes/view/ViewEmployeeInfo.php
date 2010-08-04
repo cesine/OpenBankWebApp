677513b895a076a360af1a73b94ddeac428ebcd0
zayikina <zayikina>
2010-08-04 03:31:15 +0000

    editing view employee info

--- html/includes/view/ViewEmployeeInfo.php
+++ html/includes/view/ViewEmployeeInfo.php
@@ -18,7 +18,7 @@ echo "<h4> View employee info. </h4>\n";
 	<tr><td>Select option to show:</td><td>
 		<select name="choice1" size="4">
 		
-			<option value="personalInfo">personal info</option>
+			<option value="personalInfo" selected>personal info</option>
 			<option value="workHistory">work history</option>
 			<option value="shedule">shedule</option>
 			<option value="holidays">holidays</option>
