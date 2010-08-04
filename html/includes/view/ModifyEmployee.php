89b073222fe4818e5761de4f349484aaa7b49e1e
zayikina <zayikina@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-04 22:04:21 +0000

    editing of modify employee view

--- html/includes/view/ModifyEmployee.php
+++ html/includes/view/ModifyEmployee.php
@@ -1,10 +1,18 @@
-<p></p>
 <?php 
-session_start();
+//session_start();
 include('includes/model/Employee.Class.php');
 include('includes/controller/Database.Class.php');
 include ('includes/view/employeetopmenu.php');  
-echo "<h4> Modify employee. </h4>\n";
-echo "<form action='?&content=ModifyEmployee&topMenu=EmployeeTopMenu' method='POST'>";
 
+require_once('includes/model/Branch.Class.php');
+require_once('includes/model/EmployeeTitle.Class.php');
+require_once('includes/model/EmployeeWorkHistory.Class.php');
+
+echo "<form action='?&content=ModifyEmployee&topMenu=EmployeeTopMenu' method='POST'>";
+?>
+
+<p></p>
+
+<?php 
+echo "<h4> Modify employee. </h4>\n";
 ?>
