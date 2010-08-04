bbc70ce1b749325b46ea398d19a22eba66c1474a
zayikina <zayikina@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-04 22:54:22 +0000

    editing of modify employee view

--- html/includes/view/ModifyEmployee.php
+++ html/includes/view/ModifyEmployee.php
@@ -16,3 +16,82 @@ echo "<form action='?&content=ModifyEmployee&topMenu=EmployeeTopMenu' method='PO
 <?php 
 echo "<h4> Modify employee. </h4>\n";
 ?>
+
+<p></p>
+
+<?php 
+// create new objects
+$employee = new Employee();
+$branch = new Branch();
+$employeeTitle = new EmployeeTitle();
+$employeeWorkHistory = new EmployeeWorkHistory();
+?>
+
+<!-- Select employee to show -->
+<table border="1"> 
+	<tr><td width="180">Select employee to show:</td><td width="180">
+		<select name="choice1">
+		
+		<?php 
+			
+			//Build dynamic selection list
+			$dbSelectEmployee = new Database();
+			$dbSelectEmployee->connect();
+			
+			$querySelectEmployee="SELECT DISTINCT employeeid 
+							   FROM employee";
+										
+			$dbSelectEmployee->query($querySelectEmployee);	
+			$result = $dbSelectEmployee->query($querySelectEmployee);						
+						   
+			//Put results of query into dynamic list
+			for($count=0;$count<$dbSelectEmployee->queryResultsCount;$count=$count+1)
+			{
+				$row=mysql_fetch_array($dbSelectEmployee->queryResultsResource);
+				extract($row);
+				echo "<option value='$employeeid'>$employeeid</option>";
+			
+			}//endl if to only print when there are any results
+			echo "</select>\n";	
+			$dbSelectEmployee->close();
+						
+		?>
+		
+		</select>
+	</td></tr>
+</table>
+<!-- End select employee to show -->
+
+<!-- Choose what should be shanged -->
+<table border="1"> 
+	<tr><td width="180">Select information to change:</td><td width="180">
+		<select name="choice2">
+			<option value="first name">first name</option>
+			<option value="last name">last name</option>
+			<option value="address">address</option>			
+			<option value="branch/title/salary"selected>branch/title/salary</option>
+			<option value="vacation type">vacation type</option>
+		</select>
+	</td></tr>
+</table>
+
+<!-- read input from user after submition -->
+<P></P>
+<input type="submit" name="SelectedOptionsSubmit" value="submit selection" />
+<P></P>
+
+<?php
+
+$selectedInfo=$_POST["choice2"];
+$selectedEmployee=$_POST["choice1"];
+
+//if submit selection button is pressed:
+if (isset($_POST["SelectedOptionsSubmit"])) 						// if user press login button
+{
+	
+	echo "<h4> Selected: </h4>\n";
+	echo "<h5> information to change: $selectedInfo </h5>\n";
+	echo "<h5> employee ID:           $selectedEmployee </h5>\n";
+}
+
+?>
