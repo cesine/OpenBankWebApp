251bd1d3f6144c03b73a41fdec044519d5d31c31
cesine <cesine@users.noreply.github.com>
2010-08-04 22:57:28 +0000

    fixed the logon. also set the client to Chantal	Wordsworth so that we dont have to type it

--- html/includes/view/Login.php
+++ html/includes/view/Login.php
@@ -1,23 +1,21 @@
-Please log in.
-<Form>
-<table width="240" border="0" align="center" cellpadding="1" cellspacing="3">
-							<tr>
-								<td class="fieldTitleLeftB">Bank Card Number</td>
 
-								<td><input type="text" value="453" name="69022804" size=17 maxlength=16  autocomplete="off" onKeyUp='javascript:autoTabFromSC(getFieldByNameInForm("69022804", getFormByName("LoginForm")), getFieldByNameInForm("25768052", getFormByName("LoginForm")));'></td>
-							</tr>
-							<tr>
-								<td class="fieldTitleLeftB">Password</td>
-								<td height=""><input type="password" name="25768052" size=17 maxlength=16 autocomplete="off"></td>
-							</tr>
-							<tr>
-								<td valign="middle">&nbsp;</td>
+<form action="?&content=Summary" method="post">
+<fieldset>
+<legend><font color=cornflowerblue><b>Please log in</b></font></legend>
+<table width="240" border="0" align="center" cellpadding="10"
+	cellspacing="3">
+	<tr>
+		<td class="fieldTitleLeftB">Bank Card Number</td>
 
-								<td height="35" valign="middle">
-									<input type=image name="Submit" src="/images/enterscotiaonline.gif" width="145" height="19" alt="Enter Scotia OnLine" border="0">
-								</td>
-							</tr>
-
-
-						</table>
-
+		<td><input type="text" value="54010015" name="clientid" size=17
+			maxlength=16></td>
+	</tr>
+	<tr>
+		<td class="fieldTitleLeftB">Password</td>
+		<td height=""><input type="password" name="pass" value="Hi89R!23" size=17
+			maxlength=16 autocomplete="off"></td>
+	</tr>
+</table>
+<input type="submit" name="mysubmit" value="Submit">
+</fieldset>
+</form>
