2cea92d7a4848880520975b5a6f838c4a38cfe4e
zayikina <zayikina@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-04 15:17:44 +0000

    postal code object added

---
+++ html/includes/model/PostalCodes.Class.php
@@ -0,0 +1,73 @@
+<?php
+class PostalCodes
+{
+	
+	private $postalCodes;	
+	private $street;
+	private $city;
+	private $province;
+
+	public function getPostalCodes()
+	{
+	        return $this->postalCodes;
+	}
+	
+	public function getStreet()
+	{
+	        return $this->street;
+	}
+	
+	public function getCity()
+	{
+	        return $this->city;
+	}
+	public function getProvince()
+	{
+	        return $this->province;
+	}
+
+	
+	public function setPostalCodes($postalCodesIn)
+	{
+	        $this->postalcode=$postalCodesIn;
+	}	
+
+	public function setStreet($streetIn)
+	{
+        $this->street=$streetIn;
+	}
+	public function setCity($cityIn)
+	{
+	        $this->city=$cityIn;
+	}
+	
+	public function setProvince($provinceIn)
+	{
+	        $this->province=$provinceIn;
+	}
+
+	public function initializePostalCodes($row)
+	{
+		// in the line ($row[]), parameter name [] from db table
+		$this->setPostalCodes($row[postalcodes]);
+		$this->setStreet($row[street]);
+		$this->setCity($row[city]);	
+		$this->setProvince($row[province]);			
+	}
+
+	public function displayInRowFormatted()
+	{
+		
+	echo '<TR class="bgcoloroption1">
+			<TD class="tableDataLeftC">'.$this->postalCodes.'</TD>
+			<TD class="tableDataLeftC">'.$this->province.'</TD>		
+			<TD class="tableDataLeftC">'.$this->city.'</TD>		
+			<TD class="tableDataLeftC">'.$this->street.'</TD>					
+    	  </TR>';		
+	}		
+
+
+	
+}
+
+?>
