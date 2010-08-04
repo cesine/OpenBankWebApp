d878db016447eab2cacff94ad00bcea6bdc241f8
zayikina <zayikina@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-04 01:38:05 +0000

    add employee page added

---
+++ html/includes/view/AddEmployee.php
@@ -0,0 +1,8 @@
+<?php 
+session_start();
+include('includes/model/Employee.Class.php');
+include('includes/controller/Database.Class.php');
+echo "<h4> Add employee. </h4>\n";
+echo "<form action='?&content=EmployeeLogin&topMenu=EmployeeTopMenu' method='POST'>";
+
+?>
