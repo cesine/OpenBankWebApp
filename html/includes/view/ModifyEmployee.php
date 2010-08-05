71737191faf0569038f3e21374c9961fe24983c8
zayikina <zayikina@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-05 14:19:56 +0000

    modify employee corrected

--- html/includes/view/ModifyEmployee.php
+++ html/includes/view/ModifyEmployee.php
@@ -174,7 +174,8 @@ if (isset($_POST["SelectedOptionsSubmit"]))
 		
 		<!-- read input from user after submition -->
 		<P></P>
-		<input type="submit" name="SelectedOptionsSubmit" value="submit changes" />
+		<!-- <input type="submit" name="SelectedOptionsSubmit" value="submit changes" /> -->
+		<input type="submit" name="SubmitChanges" value="submit changes" />		
 		<P></P>		
 		
 		<?php
