6dac1e3497b137b39578d1354db989632e5abda4
zayikina <zayikina>
2010-08-05 13:42:25 +0000

    add employee corrected (put instead include require_once)

--- html/includes/view/AddEmployee.php
+++ html/includes/view/AddEmployee.php
@@ -2,8 +2,8 @@
 <?php 
 session_start();
 include('includes/model/Employee.Class.php');
-include('includes/controller/Database.Class.php');
-include ('includes/view/employeetopmenu.php');  
+include ('includes/view/employeetopmenu.php'); 
+require_once('includes/controller/Database.Class.php'); 
 echo "<h4> Add employee. </h4>\n";
 echo "<form action='?&content=AddEmployee&topMenu=EmployeeTopMenu' method='POST'>";
 
