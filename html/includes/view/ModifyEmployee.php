0db3104cd751c65f19f15a17181494c39629d6e8
zayikina <zayikina@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-05 13:51:06 +0000

    modify employee corrected

--- html/includes/view/ModifyEmployee.php
+++ html/includes/view/ModifyEmployee.php
@@ -80,7 +80,7 @@ if (isset($_POST["SelectedOptionsSubmit"]))
 		//echo "<h4> Selected: </h4>\n";
 		//echo "<h5> information to change: $selectedInfo </h5>\n";
 		//echo "<h5> employee ID:           $selectedEmployee </h5>\n";
-		include('includes/model/AddClient.Class.php');		
+		require_once('includes/view/AddClient.Class.php');		
 		
 		echo "<h4> Current Info: </h4>\n";
 ?>
