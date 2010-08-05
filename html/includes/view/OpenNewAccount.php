9d819001be2b0b6e00bbcf34848241f050f40150
abisankar <abisankar@users.noreply.github.com>
2010-08-05 01:31:04 +0000

    added accounttype

--- html/includes/view/OpenNewAccount.php
+++ html/includes/view/OpenNewAccount.php
@@ -40,20 +40,28 @@
 
  -->
 <table border="1"> 
-<tr><td>Account name:</td><td><select name="accountType" size="8" single="single">
+<tr><td>Account name:</td><td><select name="accountType" size="15" single="single">
 <option value="powerChecking">Powerchequing Account</option>
 <option value="moneyMasterSavings">Money Master Savings Account</option>
 <option value="usDollarAccount">US Dollar Account</option>
 <option value="creditCard">Credit Card No-Fee Value VISA</option>
 <option value="lineOfCredits">Basic Line of Credit</option>
 <option value="rSPSix">GIC(6mos) with Flex for RSPs Account</option>
-<option value="tSFA">GIC with Flex for TFSAs Account</option>
-<option value="life">Accidental Death Insurance</option>
+<option value="rSPTwelve">GIC(12mos) with Flex for RSPs Account</option>
+<option value="rSPEighteen">GIC(18mos) with Flex for RSPs Account</option>
+<option value="rSPTFour">GIC(24mos) with Flex for RSPs Account</option>
+<option value="tSFASix">GIC with Flex for TFSAs Account</option>
+<option value="tSFAtwelve">GIC(12mos) with Flex for TFSAs Account</option>
+<option value="tSFAEighteen">GIC(18mos) with Flex for TFSAs Account</option>
+<option value="tSFATfour">GIC(24mos) with Flex for TFSAs Account</option>
+<option value="lifeOne">Accidental Death Insurance - 1</option>
+<option value="lifeTwo">Accidental Death Insurance - 2</option>
+<option value="lifeThree">Accidental Death Insurance - 3</option>
+<option value="lifeFour">Accidental Death Insurance - 4</option>
 <option value="businessChecking">Basic Business Chequing Account</option>
 <option value="businessSavings">Basic Business Savings Account</option>
 <option value="businessFC">Basic Business Foreign Currency Account</option>
-<option value="rSPtwelve">GIC(12mos) with Flex for RSPs Account</option>
-<option value="rSPEighteen">GIC(18mos) with Flex for RSPs AccountLife</option>
+
 </select>
 </td>
 </tr></table>
