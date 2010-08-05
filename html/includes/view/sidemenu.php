c990d1dc2bbe73bb65d152143a1ce67e9989c347
abisankar <abisankar@users.noreply.github.com>
2010-08-05 19:11:24 +0000

    added change

--- html/includes/view/sidemenu.php
+++ html/includes/view/sidemenu.php
@@ -26,7 +26,7 @@
 		<td width="14" height="21" align="center"><img
 		src="/images/red_bullet_trans.gif" width="6" height="7"></td>
 		<td width="124"><a
-		href="index.php?&content=OpenNewAccount"
+		href="index.php?&content=NewOpenNewAccount"
 		class="linkVertMenu">Open an Account</a></td>
 	</tr>
 
