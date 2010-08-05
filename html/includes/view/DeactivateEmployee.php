afb04df549f641d437022ec0803b422cabf0515b
zayikina <zayikina@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-05 13:43:07 +0000

    deactivate employee corrected (put instead include require_once)

--- html/includes/view/DeactivateEmployee.php
+++ html/includes/view/DeactivateEmployee.php
@@ -2,8 +2,8 @@
 <?php 
 session_start();
 include('includes/model/Employee.Class.php');
-include('includes/controller/Database.Class.php');
 include ('includes/view/employeetopmenu.php');  
+require_once('includes/controller/Database.Class.php');
 echo "<h4> Deactivate employee. </h4>\n";
 echo "<form action='?&content=DeactivateEmployee&topMenu=EmployeeTopMenu' method='POST'>";
 
