cf6c7a9789e771be05b4ed6545e485efa9d5b10f
garganshu <garganshu@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-05 20:26:57 +0000

    All the files that end with "_anshu" will be removed later on.
    This is the only way, I can play around with menus without disturbing others.

---
+++ html/includes/view/branchtopmenu_anshu.php
@@ -0,0 +1,3 @@
+<table  width="600" border="0" bgcolor="#bbb" ><tr><td>
+<a href="index.php?&content=PersonalServices&topMenu=EmployeeTopMenu">Persoanl Services</a></td><td>
+<a href="index.php?&content=BusinessServices&topMenu=EmployeeTopMenu">Business Services</a></td><td>
