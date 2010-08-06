c156c544b75b6d7c5c4f99629c69e0753f433bc2
zayikina <zayikina>
2010-08-06 20:08:49 +0000

    editing of add employee view

--- html/includes/view/AddEmployee.php
+++ html/includes/view/AddEmployee.php
@@ -8,6 +8,7 @@ require_once('includes/model/Branch.Class.php');
 require_once('includes/model/EmployeeTitle.Class.php');
 require_once('includes/model/EmployeeWorkHistory.Class.php');
 require_once('includes/model/EmployeeTimeOffPlan.Class.php');
+require_once('includes/model/Address.Class.php');
 
 echo "<form action='?&content=AddEmployee&topMenu=EmployeeTopMenu' method='POST'>";
 
@@ -21,35 +22,10 @@ echo "<form action='?&content=AddEmployee&topMenu=EmployeeTopMenu' method='POST'
 
 <p></p>
 
-<?php 
-	// create new objects
-	$employee = new Employee();
-	$branch = new Branch();
-	$employeeTitle = new EmployeeTitle();
-	$employeeWorkHistory = new EmployeeWorkHistory();
-	$employeeTimeOffPlan = new EmployeeTimeOffPlan();	
-	
-	// create dynamic list of branches and let user to select
-	$branch->BranchList();
-	
-	// create dynamic list of employee title names and let user to select
-	$employeeTitle->displayEmployeeTitleList();	
-	
-	$s="first name";
-?>
-
-	<!-- Create field to put salary -->
-	<table border="1"> 
-		<tr><td width="180">Salary:</td><td width="180">
-			<input type="text" name="choiceSalary" value=0 maxlength=8 />
-		</td></tr>
-	</table>
-	<!-- End create field to put salary -->		
-	
 	<!-- Create field to put first name -->
 	<table border="1"> 
 		<tr><td width="180">First name:</td><td width="180">
-			<input type="text" name="choiceFirstName" value=\"" .$s. "\" maxlength=20 />
+			<input type="text" name="choiceFirstName" maxlength=20 />
 		</td></tr>
 	</table>
 	<!-- End create field to put first name -->		
@@ -62,17 +38,117 @@ echo "<form action='?&content=AddEmployee&topMenu=EmployeeTopMenu' method='POST'
 	</table>
 	<!-- End create field to put last name -->	
 
+<?php 
+	// create new objects
+	$employee = new Employee();
+	$branch = new Branch();
+	$title = new EmployeeTitle();
+	$workHistory = new EmployeeWorkHistory();
+	$timeOffPlan = new EmployeeTimeOffPlan();	
+	$address = new Address();
+	
+	// create dynamic list of branches and let user to select
+	$branch->BranchList();
+	
+	// create dynamic list of employee title names and let user to select
+	$title->displayEmployeeTitleList();	
+	
+	// create dynamic list of employee's time off plan names and let user to select
+	$timeOffPlan->displayEmployeeTimeOffPlanList();		
+	
+	// create dynamic list of postal codes
+	$address->PostalCodesList();	
+	
+?>
+
+	<!-- read input from user after submition -->
+	<P></P>
+	<input type="submit" name="EmployeeInfoSubmit" value="submit selection" />
+	<P></P>
+	
 <?php
 
-	//echo "<input type=text name="choiceFirstName" value=\"" .$s. "\" maxlength=20 />";	
+	$employeeFirstName=$_POST["choiceFirstName"];
+	$employeeLastName=$_POST["choiceLastName"];	
+	$employeeBranch=$_POST["choiceBranch"];	
+	$employeeTitle=$_POST["choiceTitle"];
+	$employeeTimeOffPlan=$_POST["choiceTimeOffPlan"];	
+	$employeePostalCode=$_POST["choicePostalCode"];				
 	
+	//if submit selection button is pressed:
+	if (isset($_POST['EmployeeInfoSubmit'])) 	
+	{
+		
+	    // validate values 
+		if($employeeFirstName=="" || $employeeLastName=="")
+		{
+			echo "<h4>Please, provide first name and last name.</h4>";
+		}	
+		elseif (is_string($employeeFirstName)==false) 
+		{
+			echo "<h4>First name should be a string.</h4>";	
+		}
+		elseif (is_string($employeeLastName)==false) 
+		{
+			echo "<h4>Last name should be a string.</h4>";	
+		}		
+		else 																	// input is OK
+		{
+			//find base salary for selected title name
+			$dbSelectBaseSalary = new Database();
+			$dbSelectBaseSalary->connect();
+							
+			$querySelectBaseSalary="SELECT basesalary 
+							   		FROM employeetitle
+							   		WHERE titlename=$employeeTitle";
+														
+			$dbSelectBaseSalary->query($querySelectBaseSalary);	
+			$result = $dbSelectBaseSalary->query($querySelectBaseSalary);						
+										   
+			for($count=0;$count<$dbSelectBaseSalary->queryResultsCount;$count=$count+1)
+			{
+				$row=mysql_fetch_array($dbSelectBaseSalary->queryResultsResource);
+			}
+			
+			// save current values
+			$employeeBaseSalary=$row[basesalary];				
+
+			$dbSelectBaseSalary->close();			
+			
+			// show base salary for selected title, which can be corrected
+?>
+			<!-- Create field to put salary, by defalt base salary -->
+			<table border="1"> 
+				<tr><td width="180">Salary:</td><td width="180">
+					<?php 
+						echo "<input type=text name=\"choiceSalary\" maxlength=\"8\" value=\"" . $employeeBaseSalary . "\">";
+					?>				
+				</td></tr>
+			</table>
+			<!-- End create field to put salary -->		
+
+<?php 
+		} // end input is OK		
+	
+	} // end if (isset($_POST['EmployeeInfoSubmit'])) 
+
+
+
 	echo "<input type=text disabled name=\"choiceFirstName\" size=\"25\" maxlength=\"20\" value=\"" . $s . "\">";
 	
 
-	// create dynamic list of employee's time off plan names and let user to select
-	$employeeTimeOffPlan->displayEmployeeTimeOffPlanList();
+
 
 ?>					
+
+	<!-- Create field to put salary -->
+	<table border="1"> 
+		<tr><td width="180">Salary:</td><td width="180">
+			<input type="text" name="choiceSalary" value=0 maxlength=8 />
+		</td></tr>
+	</table>
+	<!-- End create field to put salary -->		
+	
 		
 
 
