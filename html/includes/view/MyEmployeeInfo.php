a4d65abae096ce99319f2d3deb80c83a8ec2a9a5
zayikina <zayikina>
2010-08-07 21:42:00 +0000

    my employee info page added

--- html/includes/view/MyEmployeeInfo.php
+++ html/includes/view/MyEmployeeInfo.php
@@ -1,6 +1,14 @@
 <p></p>
 <?php 
 
-	echo "<h5> My employee info. </h5><p></p>";
+	echo "<h4> My employee info. </h4><p></p>";
+	
+	if (isset($_SESSION['Employee']))
+	{
+		$employeeID=unserialize($_SESSION['Employee']);
+		$employeeID->getEmployeeID();
+		echo "<h5> Employee ID: $employeeID </h5><p></p>";		
+
+	}
 
 ?>
