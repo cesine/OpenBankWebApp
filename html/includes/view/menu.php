8e2afc09c0221fa6ad92f4056517b860c35919e5
cesine <cesine@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-07 18:47:21 +0000

    shortened top banner

--- html/includes/view/menu.php
+++ html/includes/view/menu.php
@@ -1,12 +1,12 @@
 <div id="top"> <!-- The top panel -->
 
-<table bgcolor="#555555" width="100%"><tr valign="top"><td width="60%"><li><a href="index.php">Home</a></li></td>
+<table bgcolor="#999999" width="100%"><tr valign="top"><td width="60%"><li><a href="index.php">Home</a></li></td>
 <td width="20%"><li><a href="index.php?&content=BranchLocator">Branch Locator</a></li></td>
 <td width="20%"><li><a href="index.php?&content=EmployeeLogin">Employee Login</a></li></td>
 </tr></table>
 
 <div id="section">
 <h1>Open Bank</h1>
-<p> Online Banking Made Easy: <br/>The Open Bank system is designed to model the website for any Canadian bank.</p>
+<!-- <p> Online Banking Made Easy: <br/>The Open Bank system is designed to model the website for any Canadian bank.</p> -->
 </div>
 </div>
