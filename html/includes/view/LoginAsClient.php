e8de0a63aded34feb5a9789419e6a3c92d08b255
zayikina <zayikina>
2010-08-07 20:51:37 +0000

    login as a client added

---
+++ html/includes/view/LoginAsClient.php
@@ -0,0 +1,15 @@
+<form action="?&action=LoginAsClient" method="post">
+<fieldset>
+<legend><font color=cornflowerblue><b>View Client Info - Log in as client </b></font></legend>
+<table width="240" border="0" align="center" cellpadding="10"
+	cellspacing="3">
+	<tr>
+		<td class="fieldTitleLeftB">Bank Card Number</td>
+
+		<td><input type="text" value="54010015" name="$clientid" size=8
+			maxlength=16></td>
+	</tr>
+</table>
+<input type="submit" name="$LoginAsClient" value="Submit">
+</fieldset>
+</form>
