5cd975b91e0fad39f2d7ab207fbfa93cebdb6552
cesine <cesine@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-08 14:58:18 +0000

    implemented and tested the saveToDatabase function which will put the object in the database if is not already there

--- html/includes/view/TestModel.php
+++ html/includes/view/TestModel.php
@@ -7,11 +7,16 @@ require_once 'includes/model/Client.Class.php';
 echo "<h4>Testing setting some elements in a postal code, then printing it as it would appear in an addres.</h4>";
 $p= new PostalCodes();
 $p->setCity("Montreal");
-$p->setProvince("QC");
+$p->setProvince("Quebec");
 $p->setPostalCodes("H3G-1M8");
 $p->displayPostalCodeInfo();
 
 echo "<p>The above postal code is: ".$p->getPostalCodes()." </p>";
+
+$p->setPostalCodes("H3K-1J3");
+//$successOrNot=$p->saveToDatabase();
+echo "Here is the result of the insert : ".$successOrNot;
+
 
 echo "<h4>Testing setting some elements in an address, then printing it as it would appear on a letter</h4>";
 $address = new Address();
@@ -21,9 +26,13 @@ $address->setPostalCodeFromObject($p);
 $address->displayAddress();
 
 echo "<h5>Changing the city via the address object</h5>";
-$address->setCity("Ville Marie");
+$address->setCity("Ville Marie2");
 $address->displayAddress();
 
+echo"<h5>Saving the address object (and consequently the postal code) to the database if they do not already exist.</h5>";
+$idOfInsertedAddress=$address->saveToDatabase();
+echo "Here is the result of the insert, this case its the Id of the address that was just inserted, or 0 if the address already existed : ".$idOfInsertedAddress;
+echo "<p></p>";
 
 echo "<h5>Displaying an address in a row.</h5>";
 echo "<table border=1>";
@@ -35,7 +44,7 @@ $address->PostalCodesList();
 
 
 if(isset($_SESSION['Client'])){
-	echo"<hr>";
+	echo"<hr height=10s>";
 	echo"<h4>Testing client</h4>";
 	$client= new Client();
 	$client=unserialize($_SESSION['Client']);
@@ -57,6 +66,9 @@ if(isset($_SESSION['Client'])){
 	foreach ($client->getClientAccountsArray() as $accountNumber){
 		echo "Client has Account: $accountNumber<br/>";
 	}
+	
+	
+	
 	echo "</p>";
 }
 echo "<h4>Testing employee, displaying personal info in row</h4>";
