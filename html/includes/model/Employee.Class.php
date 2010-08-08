5938053c5ecfa3df837140b81d42fa5d952bc26b
cesine <cesine@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-08 05:01:07 +0000

    found bug from ViewEmployeInfo, it was that the address was not created in the employee construtor, so when called set or get on it, it didnt work.

--- html/includes/model/Employee.Class.php
+++ html/includes/model/Employee.Class.php
@@ -4,144 +4,120 @@ require_once('includes/model/PostalCodes.Class.php');
 class Employee{
 	private $employeeID;
 	private $salary;
-	private $firstName;	
-	private $lastName;		
+	private $firstName;
+	private $lastName;
 	private $timeOffID;
 	private $status;
-	public $address;
-	private $branchID;	
-	private $titleID;	
+	private $address;
+	private $branchID;
+	private $titleID;
 	private $titleName;
-	
+
 	public function canEditHR(){
 		return ($this->titleName=="Branch Manager");
 	}
-
-	public function getEmployeeID() 
-	{
+	public function getEmployeeID(){
 		return $this->employeeID;
 	}
-	
-	public function getSalary() 
-	{
+	public function getSalary(){
 		return $this->salary;
 	}
-
-	public function getFirstName()
-	{
+	public function getFirstName(){
 		return $this->firstName;
 	}
-	
-	public function getLastName()
-	{
+	public function getLastName(){
 		return $this->lastName;
 	}
-
-	public function getTimeOffID()
-	{
+	public function getTimeOffID(){
 		return $this->timeOffID;
-	}	
-
-	public function getStatus()
-	{
+	}
+	public function getStatus(){
 		return $this->status;
-	}	
-	
-	public function getAddress()
-	{
+	}
+	public function getAddress(){
 		return $this->address;
-	}	
-	
-	public function getBranchID()
-	{
+	}
+	public function getBranchID(){
 		return $this->branchID;
-	}		
-	
-	public function getTitleID()
-	{
+	}
+	public function getTitleID(){
 		return $this->titleID;
-	}	
-	
-	
-	public function setEmployeeID($employeeID) 
-	{
+	}
+
+
+	public function setEmployeeID($employeeID){
 		$this->employeeID=$employeeID;
 	}
-	
-	public function setSalary($salary) 
-	{
+	public function setSalary($salary){
 		$this->salary=$salary;
-	}	
-	
-	public function setFirstName($firstName) 
-	{
+	}
+	public function setFirstName($firstName){
 		$this->firstName=$firstName;
-	}	
-
-	public function setLastName($lastName) 
-	{
+	}
+	public function setLastName($lastName){
 		$this->lastName=$lastName;
 	}
-
-	public function setTimeOffID($timeOffID) 
-	{
+	public function setTimeOffID($timeOffID){
 		$this->timeOffID=$timeOffID;
 	}
-
-	public function setStatus($status) 
-	{
+	public function setStatus($status){
 		$this->status=$status;
 	}
-
-	public function setAddressID($addressID) 
-	{
-		$this->addressID=$addressID;
-	}	
-	
-	public function setBranchID($branchID) 
-	{
+	public function setBranchID($branchID){
 		$this->branchID=$branchID;
 	}
-
-	public function setTitleID($titleID) 
-	{
+	public function setTitleID($titleID){
 		$this->titleID=$titleID;
-	}	
-	public function setTitleName($titleID) 
-	{
-		//go in database and get title name
+	}
+	public function setTitleName($titleID){
+		//go in database and get title name, using the titleid foreign key
 		$dbCheckBranchManager = new Database();
 		$dbCheckBranchManager->connect();
 			
 		$queryCheckBranchManager="SELECT titlename
 							   FROM employeetitle
 							   WHERE titleid=$titleID";
-										
-		$dbCheckBranchManager->query($queryCheckBranchManager);	
-		$dbCheckBranchManager->close();	
-		
+
+		$dbCheckBranchManager->query($queryCheckBranchManager);
+		$dbCheckBranchManager->close();
+
 		$this->titleName=$dbCheckBranchManager->queryFirstResult[titlename];
-				
 	}
 	public function setAddress($addressid) {
-		$this->address = new Address($addressid);
+		$this->address->initializeAddress($addressid);
 	}
-	
-	public function __construct($employeeid){
-		//build an employee object	
+	public function __construct(){
+		$this->address = new Address();
+	}	
+	public function initializeEmployee($employeeid){
+		//build an employee object using just the employee id
 		$dbConstructEmployee= new Database();
 		$dbConstructEmployee->connect();
 		$queryGetEmployee="SELECT *
-							   FROM employee
-							   WHERE employeeid=$employeeid";
+							   FROM employee e
+							   WHERE e.employeeid=$employeeid";
 		$dbConstructEmployee->query($queryGetEmployee);
 		$dbConstructEmployee->close();
-		$this->initializeEmployee($dbConstructEmployee->queryFirstResult);
+		$this->initializeEmployeeFromRow($dbConstructEmployee->queryFirstResult);
 	}
+	public function initializeEmployeeFromRow($row)
+	{
+		// in the line ($row[employeeid]), parameter name [] from db table
+		$this->setEmployeeID($row[employeeid]);
+		$this->setFirstName($row[firstname]);
+		$this->setLastName($row[lastname]);
+		$this->setBranchID($row[branchid]);
+		$this->setTitleID($row[titleid]);
+		$this->setTitleName($row[titleid]);
+		$this->setSalary($row[salary]);
+		$this->setAddress($row[addressid]);
+		$this->setTimeOffID($row[timeoffid]);
+		$this->setStatus($row[status]);
+	}
+	
 	function displayEmployeeInRowFormatted()
 	{
-		
-	echo '<TR class="bgcoloroption1">
+		echo '<TR class="bgcoloroption1">
 	<TD class="tableDataLeftC">'.$this->employeeID.'</TD>
 	<TD class="tableDataRightC">'.$this->firstName.'</TD>
 	<TD class="tableDataRightC">'.$this->lastName.'</TD>	
@@ -157,37 +133,22 @@ class Employee{
 	</TR>';		
 	}
 
-	public function initializeEmployee($row)
-	{
-		// in the line ($row[employeeid]), parameter name [] from db table
-		$this->setEmployeeID($row[employeeid]);
-		$this->setFirstName($row[firstname]);
-		$this->setLastName($row[lastname]);	
-		$this->setBranchID($row[branchid]);	
-		$this->setTitleID($row[titleid]);	
-		$this->setTitleName($row[titleid]);			
-		$this->setSalary($row[salary]);
-		$this->setAddressID($row[addressid]);
-		$this->setAddress($row[addressid]);	
-		$this->setTimeOffID($row[timeoffid]);
-		$this->setStatus($row[status]);
-	}	
 	
 	public function initializeEmployeeIDFirstLastName($row)
 	{
 		// in the line ($row[employeeid]), parameter name [] from db table
 		$this->setEmployeeID($row[employeeid]);
 		$this->setFirstName($row[firstname]);
-		$this->setLastName($row[lastname]);	
-		$this->setBranchID($row[branchid]);	
-		$this->setTitleID($row[titleid]);	
-		$this->setTitleName($row[titleid]);			
+		$this->setLastName($row[lastname]);
+		$this->setBranchID($row[branchid]);
+		$this->setTitleID($row[titleid]);
+		$this->setTitleName($row[titleid]);
 		$this->setSalary($row[salary]);
-		$this->setAddressID($row[addressid]);	
+		$this->setAddressID($row[addressid]);
 		$this->setTimeOffID($row[timeoffid]);
 		$this->setStatus($row[status]);
-	}		
-	
+	}
+
 	public function displayEmployeePersonalInfo()
 	{
 		echo '	<tr><TD class="tableDataLeftC">'.$this->employeeID.'</TD>
@@ -197,48 +158,49 @@ class Employee{
 				<TD class="tableDataRightC">'.$this->address->getStreet().'</TD>
 				<TD class="tableDataRightC">'.$this->address->getCity().'</TD>
 				<TD class="tableDataRightC">'.$this->address->getProvince().'</TD>
-				<TD class="tableDataRightC">'.$this->address->getPostalCodes().'</TD></tr>';	
+				<TD class="tableDataRightC">'.$this->address->getPostalCode().'</TD></tr>';	
 			
-	}	
-	
+	}
+
 	public function EmployeeList()
 	{
-?>
-		<!-- Select employee to show -->
-		<table border="1"> 
-			<tr><td width="180">Select employee to show:</td><td width="180">
-				<select name="choiceEmployee">
-				
-				<?php 
-					
-					//Build dynamic selection list
-					$dbSelectEmployee = new Database();
-					$dbSelectEmployee->connect();
-					
-					$querySelectEmployee="SELECT DISTINCT employeeid 
+		?>
+<!-- Select employee to show -->
+<table border="1">
+	<tr>
+		<td width="180">Select employee to show:</td>
+		<td width="180"><select name="choiceEmployee">
+
+		<?php
+			
+		//Build dynamic selection list
+		$dbSelectEmployee = new Database();
+		$dbSelectEmployee->connect();
+			
+		$querySelectEmployee="SELECT DISTINCT employeeid
 									   FROM employee";
-												
-					$dbSelectEmployee->query($querySelectEmployee);	
-					$result = $dbSelectEmployee->query($querySelectEmployee);						
-								   
-					//Put results of query into dynamic list
-					for($count=0;$count<$dbSelectEmployee->queryResultsCount;$count=$count+1)
-					{
-						$row=mysql_fetch_array($dbSelectEmployee->queryResultsResource);
-						extract($row);
-						echo "<option value='$employeeid'>$employeeid</option>";
-					
-					}//endl if to only print when there are any results
-					echo "</select>\n";	
-					$dbSelectEmployee->close();
-								
-				?>
-				
-				</select>
-			</td></tr>
-		</table>
-		<!-- End select employee to show -->
-<?php 		
+
+		$dbSelectEmployee->query($querySelectEmployee);
+		$result = $dbSelectEmployee->query($querySelectEmployee);
+			
+		//Put results of query into dynamic list
+		for($count=0;$count<$dbSelectEmployee->queryResultsCount;$count=$count+1)
+		{
+			$row=mysql_fetch_array($dbSelectEmployee->queryResultsResource);
+			extract($row);
+			echo "<option value='$employeeid'>$employeeid</option>";
+
+		}//endl if to only print when there are any results
+		echo "</select>\n";
+		$dbSelectEmployee->close();
+
+		?>
+
+		</select></td>
+	</tr>
+</table>
+<!-- End select employee to show -->
+		<?php
 	} // end public function EmployeeList()
 }
 
--- html/includes/view/DeactivateEmployee.php
+++ html/includes/view/DeactivateEmployee.php
@@ -1,9 +1,5 @@
 <p></p>
 <?php 
-session_start();
-include('includes/model/Employee.Class.php');
-include ('includes/view/employeetopmenu.php');  
-require_once('includes/controller/Database.Class.php');
 echo "<h4> Deactivate employee. </h4>\n";
 echo "<form action='?&content=DeactivateEmployee&topMenu=EmployeeTopMenu' method='POST'>";
 
