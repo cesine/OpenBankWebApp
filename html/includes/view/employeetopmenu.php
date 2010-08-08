25f507066bfa4010d5d90cd55a92bcb26110176e
cesine <cesine@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-08 20:25:28 +0000

    adjusted the formating to make the color scheme more consistant

--- html/includes/view/employeetopmenu.php
+++ html/includes/view/employeetopmenu.php
@@ -1,5 +1,6 @@
-<table  width="600" border="0" bgcolor="#bbb" ><tr><td>
+<nav><table  width="650" border="0" bgcolor="#bbb" ><tr><td>
 <a href="index.php?&content=EmployeeInfo">Employees Info</a></td><td>
 <a href="index.php?&content=LoginAsClient">Clients Info</a></td><td>
 <!--  <a href="">Branch Info</a></td><td> -->
 <a href="index.php?&content=MyEmployeeInfo">My Employee Info</a></td></tr></table>
+</nav>
--- html/includes/view/topmenu.php
+++ html/includes/view/topmenu.php
@@ -1,6 +1,7 @@
-<table  width="600" border="0" bgcolor="#bbb" ><tr><td>
+<nav><table  width="650" border="0" bgcolor="#bbb" ><tr><td>
 <a href="index.php?&content=PersonalServices">Personal Services</a></td><td>
 <a href="index.php?&content=BusinessServices">Business Services</a></td><td>
 </td>
 </tr>
 </table>
+</nav>
