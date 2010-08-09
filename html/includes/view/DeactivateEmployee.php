59b1688cae775f9cb7e97178efcdfbea32c5cb98
zayikina <zayikina@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-09 10:00:29 +0000

    editing  Deactivate Employee

--- html/includes/view/DeactivateEmployee.php
+++ html/includes/view/DeactivateEmployee.php
@@ -1,6 +1,9 @@
 <p></p>
 <?php 
-echo "<h4> Deactivate employee. </h4>\n";
+	echo "<h4> Deactivate employee. </h4>\n";
+?>
+	<p></p>
+<?php 
 //echo "<form action='?&content=DeactivateEmployee&topMenu=EmployeeTopMenu' method='POST'>";
 echo "<form action='?&content=DeactivateEmployee' method='POST'>";
 
@@ -11,7 +14,7 @@ $employee->EmployeeList();
 
 <!-- read input from user after submition -->
 <P></P>
-<input type="submit" name="SelectEmployee" value="select employee" />
+<input type="submit" name="DeactivateEmployee" value="deactivate employee" />
 <P></P>
 
 <?php
@@ -22,13 +25,13 @@ $employee = new Employee();
 $employee->initializeEmployee($selectedEmployee);
 
 //if submit selection button is pressed:
-if (isset($_POST["SelectEmployee"])) 						
+if (isset($_POST["DeactivateEmployee"])) 						
 {
 	$employee->changeStatus($selectedEmployee);
 ?>
 	<P></P>
 <?php 
-	echo "<h4> Employee with ID $selectedEmployee not active. </h4>\n";
+	echo "<h4> Employee with ID $selectedEmployee is deactivated!. </h4>\n";
 	
 } // end if (isset($_POST["SelectEmployee"])) 	
 
