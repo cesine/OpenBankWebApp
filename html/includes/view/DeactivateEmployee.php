cdab622f3807014f74d5503f08f0c8dc8c1712f6
zayikina <zayikina@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-09 09:42:11 +0000

    make interface for Deactivate Employee

--- html/includes/view/DeactivateEmployee.php
+++ html/includes/view/DeactivateEmployee.php
@@ -1,6 +1,31 @@
 <p></p>
 <?php 
 echo "<h4> Deactivate employee. </h4>\n";
-echo "<form action='?&content=DeactivateEmployee&topMenu=EmployeeTopMenu' method='POST'>";
+//echo "<form action='?&content=DeactivateEmployee&topMenu=EmployeeTopMenu' method='POST'>";
+echo "<form action='?&content=DeactivateEmployee' method='POST'>";
+
+
+// create dynamic list of employee and let user to select
+$employee->EmployeeList(); 	
+?>
+
+<!-- read input from user after submition -->
+<P></P>
+<input type="submit" name="SelectEmployee" value="select employee" />
+<P></P>
+
+<?php
+
+$selectedEmployee=$_POST["choiceEmployee"];
+
+$employee = new Employee();
+$employee->initializeEmployee($selectedEmployee);
+
+//if submit selection button is pressed:
+if (isset($_POST["SelectEmployee"])) 						
+{
+	
+} // end if (isset($_POST["SelectEmployee"])) 	
+
 
 ?>
