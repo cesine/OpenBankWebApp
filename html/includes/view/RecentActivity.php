806dce707ba429f84413b41433c7825cda33c274
cesine <cesine@users.noreply.github.com>
2010-08-10 04:18:31 +0000

    added recent activty information layout

---
+++ html/includes/view/RecentActivity.php
@@ -0,0 +1,32 @@
+
+<table bgcolor="#a50000" border="0" cellpadding="0" cellspacing="0"
+	width="600">
+	<tbody>		<tr bgcolor="#ffffff">
+			<td width="58%"><font class="regularTextBold">Transaction Detail for:
+			<font color="#666666">&nbsp;</font>Current Period</font></td>
+			<td class="tableTitleRightC" width="42%"><span
+				class="tableDataRightC"> </span></td>
+		</tr>
+	</tbody>
+</table><TABLE border="0" cellspacing="1" cellpadding="3" width="100%">
+	<TR class="bgcoloroption0">
+		<td class="fieldTitleLeftC" valign=top>Date<br></td>
+		<TD class="fieldTitleCenterC" valign=top>Num</TD>
+		<td class="fieldTitleLeftC" valign=top>Description<br></td>
+		<TD class="fieldTitleRightC" valign=top>Withdrawals</TD>
+		<TD class="fieldTitleRightC" valign=top>Deposits</TD>
+		<TD class="fieldTitleRightC" valign=top>Balance</TD>
+	</TR>
+	<TR VALIGN="top" class='bgcoloroption1'>
+		<TD class="tableDataLeftC" nowrap>2010-08-04</TD>
+		<TD class="tableDataCenterC">-</TD>		<TD class="tableDataLeftC">Customer Transfer Dr.<br>
+		PC TO 453...15</TD>		<TD class="tableDataRightC" align=right>$1,805.00</TD>		<TD class="tableDataCenterC">-</TD>		<TD class="tableDataCenterC">-</TD>	</TR>	<TR VALIGN="top" class='bgcoloroption2'>
+		<TD class="tableDataLeftC" nowrap>2010-08-04</TD>
+		<TD class="tableDataCenterC">-</TD>
+		<TD class="tableDataLeftC">Customer Transfer Dr.<br>
+		PC TO 800...29</TD>		<TD class="tableDataRightC" align=right>$1,820.00</TD>		<TD class="tableDataCenterC">-</TD>		<TD class="tableDataRightC" align=right>$7.10</TD>	</TR>	<TR VALIGN="top" class="bgcoloroption2">
+		<TD colspan="3" class="fieldTitleRightC">Total :</TD>
+		<TD class="totalBalanceC" align=right>&nbsp;$3,625.00</TD>
+		<TD class="totalBalanceC" align=right>&nbsp;</TD>
+		<TD class="tableDataCenterC">&nbsp;</TD>
+	</TR></TABLE>
