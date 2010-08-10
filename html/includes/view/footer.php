19fd926c69609cb78c50ba72b677e75e8627f9c8
cesine <cesine@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-10 10:27:15 +0000

    added a "change the date" form on the bottom of the page so that the monthly charges can be tested

--- html/includes/view/footer.php
+++ html/includes/view/footer.php
@@ -2,6 +2,12 @@
 	<section id="about">
 	<div>
 		<p>&copy; Team 1	</p>
+		<form action='' method='post'>
+		Advance Date by days: (ex. 30)
+		<input value='30' autocomplete='OFF' maxlength='12' size='12'
+					name='amountofdays' type='text'>
+		<input type='submit' name='mysubmit' value='Submit'>
+		</form>
 		</div>
 	</section>
 </footer>
