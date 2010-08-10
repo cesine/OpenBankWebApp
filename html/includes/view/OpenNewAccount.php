19acb083e59ac3bb840ed6a220fc15b5926646e3
abisankar <abisankar@users.noreply.github.com>
2010-08-10 02:25:25 +0000

    trying to set values from view page

--- html/includes/view/OpenNewAccount.php
+++ html/includes/view/OpenNewAccount.php
@@ -177,9 +177,9 @@ switch ($userChoice)
 $curDate = date("Y-m-d");
 $clientAccount->setOpeningDate($curDate);
 $clientAccount->setBranchId($userBranch);
-//$clientAccount->setclientId($clientId);
-
-
+$clientAccount->setclientId($clientId);
+$clientAccount->setCurrentBalance('0');
+$clientAccount->setAvailableBalance('0');
 //inserting in the table
 echo $clientAccount->saveToDatabase();
 
