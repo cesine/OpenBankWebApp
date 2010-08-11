2fa123beca61717d92849477d6f7bccbbe7fb153
cesine <cesine@users.noreply.github.com>
2010-08-11 21:54:43 +0000

    added a loop

--- html/databasebackup/chargemonthlyfee.sql
+++ html/databasebackup/chargemonthlyfee.sql
@@ -9,6 +9,7 @@ DELIMITER $$
 CREATE  PROCEDURE `chargemonthly`()
 
 BEGIN
+	DECLARE	done INT DEFAULT 0;
 	DECLARE acctype INT;
 	DECLARE monthlycharge DECIMAL;
 	DECLARE feetype INT;
@@ -17,29 +18,29 @@ BEGIN
 	DECLARE account INT;
 	DECLARE q TEXT;
 	DECLARE cursor1 CURSOR FOR SELECT a.clientaccountid, a.currentbalance, b.monthlyfee FROM `clientaccount` a, `bankingplans` b WHERE a.accounttypeid=b.accounttypeid AND a.accounttypeid=1;
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done=1;
+
  	SET newbalance =2;
         SET account=210004074;
 	SET newbalance= 3;
 	open cursor1;
 REPEAT
 	FETCH cursor1 INTO account, balance, monthlycharge;
- 
 	SET newbalance=balance + monthlycharge;	
 
-	SET q  = CONCAT("INSERT INTO  transaction 
+	INSERT INTO  transaction 
 	(`transactionid` ,`branchid` ,`accountid` , 
 	`date` ,`transactionfeecharged`,`transactionfeetype` ,	
 	`depositamount` ,`withdrawalamount` , `balanceaftertransaction` ,
 	`transactiondescription` , `transperformedby`)	
 	VALUES 
-	(NULL,10001,",account,",",
-	CURDATE(),",",monthlycharge,",'fee',
-	NULL,",monthlycharge,",",newbalance,",
-	'Monthly Fee', 20000001);");
+	(NULL,10001,account,
+	CURDATE(),monthlycharge,'fee',
+	'NULL',monthlycharge,newbalance,
+	'Monthly Fee', 20000001);
 
-	PREPARE ps_q FROM @q;
-	EXECUTE ps_q;
-	DEALLOCATE PREPARE ps_q;
+	UPDATE clientaccount SET `currentbalance` = newbalance WHERE `clientaccoutid`=account;
+
 UNTIL done END REPEAT;
 END 
 $$
