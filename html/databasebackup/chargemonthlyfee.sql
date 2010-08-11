55efa534b1df586f9891a08525c5880d766e45e7
cesine <cesine@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-11 21:33:06 +0000

    added curser to do get values from a query

--- html/databasebackup/chargemonthlyfee.sql
+++ html/databasebackup/chargemonthlyfee.sql
@@ -10,36 +10,37 @@ CREATE  PROCEDURE `chargemonthly`()
 
 BEGIN
 	DECLARE acctype INT;
-	DECLARE monthcharge DECIMAL;
+	DECLARE monthlycharge DECIMAL;
 	DECLARE feetype INT;
+	DECLARE balance DECIMAL;
 	DECLARE newbalance DECIMAL;
-	DECLARE sec INT;	
 	DECLARE account INT;
-	DECLARE cursor1 CURSOR FOR SELECT clientaccountid FROM `clientaccount` WHERE accounttypeid=1;
- 	SET @newbalance =2;
-        SET @fee=3;
+	DECLARE q TEXT;
+	DECLARE cursor1 CURSOR FOR SELECT a.clientaccountid, a.currentbalance, b.monthlyfee FROM `clientaccount` a, `bankingplans` b WHERE a.accounttypeid=b.accounttypeid AND a.accounttypeid=1;
+ 	SET newbalance =2;
         SET account=210004074;
-
+	SET newbalance= 3;
 	open cursor1;
-	FETCH cursor1 INTO account;
+REPEAT
+	FETCH cursor1 INTO account, balance, monthlycharge;
  
-	
-	SET @newbalance =2;
-	SET @fee=3;
-	SET @q  = CONCAT("INSERT INTO  transaction 
+	SET newbalance=balance + monthlycharge;	
+
+	SET q  = CONCAT("INSERT INTO  transaction 
 	(`transactionid` ,`branchid` ,`accountid` , 
 	`date` ,`transactionfeecharged`,`transactionfeetype` ,	
 	`depositamount` ,`withdrawalamount` , `balanceaftertransaction` ,
 	`transactiondescription` , `transperformedby`)	
 	VALUES 
-	(NULL,10001,",@account,",",
-	CURDATE(),",",@fee,",'fee',
-	NULL,",@fee,",",@newbalance,",
+	(NULL,10001,",account,",",
+	CURDATE(),",",monthlycharge,",'fee',
+	NULL,",monthlycharge,",",newbalance,",
 	'Monthly Fee', 20000001);");
 
 	PREPARE ps_q FROM @q;
 	EXECUTE ps_q;
 	DEALLOCATE PREPARE ps_q;
+UNTIL done END REPEAT;
 END 
 $$
 DELIMITER ;
