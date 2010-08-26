1bad69a49621679b50719256fc65f326934565ea
cesine <cesine@f2889d19-b673-46c6-8d93-0fe199765b62>
2010-08-26 21:56:42 +0000

    deleting

--- html/includes/view/branchtopmenu_anshu.php
+++
@@ -1,3 +0,0 @@
-<table  width="600" border="0" bgcolor="#bbb" ><tr><td>
-<a href="index.php?&content=PersonalServices&topMenu=EmployeeTopMenu">Persoanl Services</a></td><td>
-<a href="index.php?&content=BusinessServices&topMenu=EmployeeTopMenu">Business Services</a></td><td>
